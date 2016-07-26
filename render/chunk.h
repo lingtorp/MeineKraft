9d826405b424b9ed44484a6c115e174336109879
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-07-26 10:41:35 +0200

    Increased chunk size

--- render/chunk.h
+++ render/chunk.h
@@ -8,8 +8,8 @@ class Chunk {
 public:
     Chunk(Vec3 world_position);
 
-    static const uint16_t BLOCKS_PER_CHUNK = 2 * 2 * 2; // Cubic root must be an integer
-    static const uint16_t dimension = 2;
+    static const uint16_t BLOCKS_PER_CHUNK = 5 * 5 * 5; // Cubic root must be an integer
+    static const uint16_t dimension = 5;
     Vec3 position;
     uint16_t numCubes;
     Vec3 dimensions;
