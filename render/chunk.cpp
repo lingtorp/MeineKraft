f33b5d653b8fcfca77d04242c8ce5b2b3afa46d6
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-07-30 23:51:50 +0200

    Chunk is now using noise to generate heights..

--- render/chunk.cpp
+++ render/chunk.cpp
@@ -1,15 +1,16 @@
 #include <cmath>
 #include <iostream>
 #include "chunk.h"
-#include "../math/noise.h"
 
 /// @param world_position World coordinates
 Chunk::Chunk(Vec3 world_position):
         position(world_position), blocks{}, numCubes(0) {
+    static auto noise = Noise();
     for (size_t x = 0; x < dimension; x++) {
         for (size_t z = 0; z < dimension; z++) {
-            auto height = std::ceil(noise::perlin(x, z) * 10);
-            std::cout << "Height: " << height << std::endl;
+            auto height = std::floor(noise.perlin(x, z, world_position, dimension) * 10);
+            std::cout << "Height: " << height;
+            std::cout << " / Noise: " << noise.perlin(x, z, world_position, dimension) << std::endl;
             for (size_t y = 0; y < height; y++) {
                 Cube cube = Cube();
                 cube.position.x = x + position.x;
--- render/chunk.h
+++ render/chunk.h
@@ -3,11 +3,12 @@
 
 #include "../math/vector.h"
 #include "primitives.h"
+#include "../math/noise.h"
 
 class Chunk {
 public:
     Chunk(Vec3 world_position);
-    static const uint16_t dimension = 5; // The 'width' of the chunk in number of cubes
+    static const uint16_t dimension = 2; // The 'width' of the chunk in number of cubes
     Vec3 position;
     uint16_t numCubes;
     std::vector<Cube> blocks;
