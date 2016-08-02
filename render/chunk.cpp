0cf142f5ce16e6ec78a1517cdb9859878799576f
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-08-02 16:56:42 +0200

    Chunks can now add negative heights..

--- render/chunk.cpp
+++ render/chunk.cpp
@@ -6,15 +6,17 @@
 Chunk::Chunk(Vec3 world_position):
         position(world_position), blocks{}, numCubes(0) {
     static auto noise = Noise();
+    static auto bottom = -3;
     for (size_t x = 0; x < dimension; x++) {
         for (size_t z = 0; z < dimension; z++) {
             auto X = x + position.x;
             auto Z = z + position.z;
-            auto height = std::round(noise.perlin(X, Z, world_position, dimension));
+            auto height = std::round(std::sqrt(noise.perlin(X, Z, world_position, dimension)));
+//            auto height = noise.generate(X, Z);
             std::cout << "Height: " << height;
             std::cout << " / Noise: " << noise.perlin(X, Z, world_position, dimension);
             std::cout << " for (x, z) = (" << X << ", " << Z << ")" << std::endl;
-            for (size_t y = 0; y < height; y++) {
+            for (auto y = bottom; y < height; y++) {
                 Cube cube = Cube();
                 cube.position.x = x + position.x;
                 cube.position.y = y + position.y;
