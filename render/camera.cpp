eb035deccbe4ebd50ec18b6bffbc1689cf96e0e2
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-08-11 12:03:39 +0200

    Simplified Camera class from the Vec* API changes.

--- render/camera.cpp
+++ render/camera.cpp
@@ -1,34 +1,30 @@
 #include "camera.h"
 
-Vec3 Camera::move_forward(double delta) const {
-    Vec3 movement =
-            vec_scalar_multiplication(direction, movement_speed * delta);
-    return vec_addition(position, movement);
+Vec3<> Camera::move_forward(double delta) const {
+    Vec3<> movement = direction * (movement_speed * delta);
+    return position + movement;
 }
 
-Vec3 Camera::move_backward(double delta) const {
-    Vec3 movement =
-            vec_scalar_multiplication(direction, movement_speed * delta);
-    return vec_subtraction(position, movement);
+Vec3<> Camera::move_backward(double delta) const {
+    Vec3<> movement = direction * (movement_speed * delta);
+    return position - movement;
 }
 
-Vec3 Camera::move_right(double delta) const {
-    Vec3 movement = normalize(cross(direction, up));
-    return vec_addition(position,
-                        vec_scalar_multiplication(movement, movement_speed * delta));
+Vec3<> Camera::move_right(double delta) const {
+    Vec3<> movement = direction.cross(up).normalize();
+    return position + (movement * (movement_speed * delta));
 }
 
-Vec3 Camera::move_left(double delta) const {
-    Vec3 movement = normalize(cross(direction, up));
-    return vec_subtraction(
-            position, vec_scalar_multiplication(movement, movement_speed * delta));
+Vec3<> Camera::move_left(double delta) const {
+    Vec3<> movement = direction.cross(up).normalize();
+    return position - (movement * (movement_speed * delta));
 }
 
-Vec3 Camera::recalculate_direction() const {
-    float rad = M_PI / 180;
-    Vec3 result;
+Vec3<> Camera::recalculate_direction() const {
+    static constexpr double rad = M_PI / 180;
+    Vec3<> result;
     result.x = -sin(yaw * rad) * cos(pitch * rad);
     result.y = sin(pitch * rad);
     result.z = -cos(yaw * rad) * cos(pitch * rad);
-    return normalize(result);
+    return result.normalize();
 }
--- render/camera.h
+++ render/camera.h
@@ -5,19 +5,19 @@
 
 class Camera {
 public:
-    Vec3 direction, position, up;
+    Vec3<GLfloat> direction, position, up;
     GLfloat pitch, yaw;
     GLfloat movement_speed; // meters per milliseconds
 
-    Camera(Vec3 position, Vec3 direction, Vec3 world_up):
+    Camera(Vec3<> position, Vec3<> direction, Vec3<> world_up):
             position(position), direction(direction), up(world_up),
-            pitch(0), yaw(0), movement_speed(0.05) {};
+            pitch(0), yaw(0), movement_speed(0.01) {};
 
-    Vec3 move_forward(double delta) const;
-    Vec3 move_backward(double delta) const;
-    Vec3 move_right(double delta) const;
-    Vec3 move_left(double delta) const;
-    Vec3 recalculate_direction() const;
+    Vec3<> move_forward(double delta) const;
+    Vec3<> move_backward(double delta) const;
+    Vec3<> move_right(double delta) const;
+    Vec3<> move_left(double delta) const;
+    Vec3<> recalculate_direction() const;
 };
 
 #endif //MEINEKRAFT_CAMERA_H
