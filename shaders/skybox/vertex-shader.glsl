b3e31782a4b98c493e9b9814ac533ab3371a51a6
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-08-16 23:01:27 +0200

    Renamned camera matrix

--- shaders/skybox/vertex-shader.glsl
+++ shaders/skybox/vertex-shader.glsl
@@ -6,7 +6,7 @@ in vec3 position;
 uniform mat4 model;
 
 // View
-uniform mat4 camera_view;
+uniform mat4 camera;
 
 // Projection
 uniform mat4 projection;
@@ -14,6 +14,6 @@ uniform mat4 projection;
 out vec3 fTexcoord;
 
 void main() {
-  gl_Position = projection * camera_view * model * vec4(position, 1.0f);
+  gl_Position = projection * camera * model * vec4(position, 1.0f);
   fTexcoord = normalize(position);
 }
