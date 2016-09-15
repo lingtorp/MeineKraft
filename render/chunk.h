16dde579087e4aafa7278d07cc44490179160bfa
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-09-15 11:46:04 +0200

    Chunk size of 32 gives nice Perlin noise distr.

--- render/chunk.h
+++ render/chunk.h
@@ -8,7 +8,7 @@
 class Chunk {
 public:
     Chunk(Vec3<> world_position);
-    static const uint16_t dimension = 6; // The 'width' of the chunk in number of cubes
+    static const uint16_t dimension = 32; // The 'width' of the chunk in number of cubes
     Vec3<> position;
     uint16_t numCubes;
     std::vector<Cube> blocks;
