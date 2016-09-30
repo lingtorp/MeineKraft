f9f2530964c35ba8192e3bc34c19a977959b2628
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-09-30 19:04:29 +0200

    Added a couple of sample ImGui widgets

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "render/render.h"
+#include "include/imgui/imgui.h"
+#include "include/imgui/imgui_impl_sdl.h"
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -8,13 +10,16 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_Window *window = SDL_CreateWindow(
-            "", 0, 0, 1000, 1000,
-            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_CAPTURE);
+    auto window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_CAPTURE;
+    SDL_Window *window = SDL_CreateWindow("MeineKraft", 0, 0, 1000, 1000, window_flags);
     SDL_GLContext context = SDL_GL_CreateContext(window);
     atexit(IMG_Quit);
     IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG); // init sdl2_image
 
+    // Init ImGui
+    ImGui_ImplSdlGL3_Init(window);
+
+    // Inits glew
     Render render{window};
 
     World world{};
@@ -26,11 +31,12 @@ int main() {
         current_tick = SDL_GetTicks();
         delta = current_tick - last_tick;
         last_tick = current_tick;
-        printf("Delta: %u ms \n", delta);
+        SDL_Log("Delta: %u ms \n", delta);
 
         /// Process input
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
+            ImGui_ImplSdlGL3_ProcessEvent(&event);
             switch (event.type) {
                 case SDL_MOUSEMOTION:
                     render.camera->pitch += event.motion.yrel;
@@ -66,16 +72,54 @@ int main() {
             }
         }
 
+        ImGui_ImplSdlGL3_NewFrame(window);
+
         /// Tick/update the world
         world.world_tick(delta, render.camera);
 
         /// Render the world
         render.render_world(&world);
 
+        /// ImGui - Debug instruments
+        {
+            auto clear_color = ImColor(114, 144, 154);
+            auto show_test_window = true;
+            auto show_another_window = true;
+            // 1. Show a simple window
+            // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+            {
+                static float f = 0.0f;
+                ImGui::Text("Hello, world!");
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+                ImGui::ColorEdit3("clear color", (float *) &clear_color);
+                if (ImGui::Button("Test Window")) show_test_window ^= 1;
+                if (ImGui::Button("Another Window")) show_another_window ^= 1;
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                            ImGui::GetIO().Framerate);
+            }
+
+            // 2. Show another simple window, this time using an explicit Begin/End pair
+            if (show_another_window) {
+                ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
+                ImGui::Begin("Another Window", &show_another_window);
+                ImGui::Text("Hello");
+                ImGui::End();
+            }
+
+            // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+            if (show_test_window) {
+                ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+                ImGui::ShowTestWindow(&show_test_window);
+            }
+        }
+
+        ImGui::Render();
         SDL_GL_SwapWindow(window);
     }
 
+    ImGui_ImplSdlGL3_Shutdown();
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
