0fedcb1e9a64c53e736a0e560962aed282b51007
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-03 23:46:45 +0200

    Window now resizeable!

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "render/render.h"
 #include "include/imgui/imgui_impl_sdl.h"
+#include <SDL2/SDL_image.h>
+#include "render/camera.h"
+#include "world/world.h"
+#include "nodes/Skybox.h"
+
+struct Resolution {
+    int width, height;
+};
+
+static auto HD      = Resolution{1280, 720};
+static auto FULL_HD = Resolution{1920, 1080};
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -10,16 +21,22 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     auto window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_CAPTURE;
-    SDL_Window *window = SDL_CreateWindow("MeineKraft", 0, 0, 1000, 1000, window_flags);
+    SDL_Window *window = SDL_CreateWindow("MeineKraft", 0, 0, HD.width, HD.height, window_flags);
     SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    // Init sdl2_image
     atexit(IMG_Quit);
-    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG); // init sdl2_image
+    IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
 
     // Init ImGui
     ImGui_ImplSdlGL3_Init(window);
 
     // Inits glew
-    Render render{window};
+    Render &render = Render::instance();
+    render.window = window;
+    render.update_projection_matrix(70);
+
+    Skybox skybox{};
 
     // Init the world with seed
     World world{1};
@@ -62,7 +79,7 @@ int main() {
                 case SDL_WINDOWEVENT:
                     switch (event.window.event) {
                         case SDL_WINDOWEVENT_RESIZED:
-                            render.update_projection_matrix();
+                            render.update_projection_matrix(70);
                             break;
                     }
                     break;
@@ -87,8 +104,8 @@ int main() {
             static bool show_test_window;
 
             ImGui::Begin("Render state");
-            ImGui::Text("Chunks: %i", world.chunks.size());
-            ImGui::Text("Entities: %i", render.state.entities);
+            ImGui::Text("Chunks: %li", world.chunks.size());
+            ImGui::Text("Entities: %lli", render.state.entities);
             ImGui::Text("Application average %u ms/frame (%.1f FPS)", delta, io.Framerate);
             if (ImGui::Button("ImGui Palette")) show_test_window ^= 1;
             ImGui::End();
