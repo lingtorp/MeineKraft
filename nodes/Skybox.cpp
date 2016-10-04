5120d210aad7d8c8a8a5a720ffdb5a940617acc5
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-04 21:18:20 +0200

    Added update method on Skybox

--- nodes/Skybox.cpp
+++ nodes/Skybox.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
 #include "Skybox.h"
-#include "../render/rendercomponent.h"
+#include "../render/camera.h"
 
-Skybox::Skybox(): Entity(), render_comp(RenderComponent{this}) {}
+Skybox::Skybox(): Entity(generate_entity_id()), render_comp{this} {
+    render_comp.set_cube_map_texture(Texture::SKYBOX);
+    scale = 300;
+}
 
 Skybox::~Skybox() {
     render_comp.remove_component();
+}
+
+void Skybox::update(uint64_t delta, const std::shared_ptr<Camera> camera) {
+    /// Update all components
+    position = camera->position;
+    // render_comp.update(delta)
 }
