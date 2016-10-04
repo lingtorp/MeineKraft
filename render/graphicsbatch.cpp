abd82a7608f364aa1428dcb425ded48a20802559
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-04 21:17:45 +0200

    Added more default inits

--- render/graphicsbatch.cpp
+++ render/graphicsbatch.cpp
@@ -1,4 +1,5 @@
 #include "graphicsbatch.h"
 #include "rendercomponent.h"
 
-GraphicsBatch::GraphicsBatch(uint64_t hash_id): hash_id(hash_id), components{}, mesh{} {};
+GraphicsBatch::GraphicsBatch(uint64_t hash_id): hash_id(hash_id), components{}, mesh{}, gl_camera_view(0),
+                                                gl_models_buffer_object(0), gl_shader_program(0), gl_VAO(0) {};
