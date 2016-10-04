6c411f14b5c1cd95d80d60faa20f0b1118b95f20
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-04 21:20:58 +0200

    Migrated to new style of batched rendering.
    Still now done yet, lots of small tweaks to be made and some large ones
    aswell.

--- render/render.cpp
+++ render/render.cpp
@@ -131,9 +131,6 @@ Renderer::Renderer(): DRAW_DISTANCE(200), projection_matrix(Mat4<float>()), stat
                                              base + std::string("res/sky/front.jpg")};
     this->textures[Texture::SKYBOX] = load_cube_map(skybox_faces, jpg);
 
-    /** Skybox **/
-    skybox.scale = 500.0f;
-
     /// Compile shaders
     // Files are truly horrible and filepaths are even worse, therefore this is not scalable
     std::string shader_base_filepath = "/Users/AlexanderLingtorp/Google Drive/Repositories/MeineKraft/shaders/";
@@ -154,80 +151,6 @@ Renderer::Renderer(): DRAW_DISTANCE(200), projection_matrix(Mat4<float>()), stat
     shaders.insert({ShaderType::STANDARD_SHADER, std_shader});
     if (!success) { SDL_Log("%s", err_msg.c_str()); }
 
-    /// Skybox vertex bindings
-    glGenVertexArrays(1, (GLuint *) &gl_skybox_VAO);
-    glBindVertexArray(gl_skybox_VAO);
-
-    this->gl_skybox_model  = glGetUniformLocation(shaders.at(ShaderType::SKYBOX_SHADER).gl_program, "model");
-    this->gl_skybox_camera = glGetUniformLocation(shaders.at(ShaderType::SKYBOX_SHADER).gl_program, "camera");
-
-    GLuint skybox_VBO;
-    glGenBuffers(1, &skybox_VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, skybox_VBO);
-    auto sky_vector = skybox.to_floats();
-    glBufferData(GL_ARRAY_BUFFER, skybox.byte_size_of_vertices(), sky_vector.data(), GL_STATIC_DRAW);
-
-    // Then set our vertex attributes pointers, only doable AFTER linking
-    auto sky_positionAttrib = glGetAttribLocation(shaders.at(ShaderType::SKYBOX_SHADER).gl_program, "position");
-    glVertexAttribPointer(sky_positionAttrib, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>),
-                          (const void *)offsetof(Vertex<float>, position));
-    glEnableVertexAttribArray(sky_positionAttrib);
-
-    GLuint sky_EBO;
-    glGenBuffers(1, &sky_EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sky_EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, skybox.byte_size_of_indices(), skybox.indices.data(), GL_STATIC_DRAW);
-
-    /** Cube **/
-    // Bind Vertex Array Object - OpenGL core REQUIRES a VERTEX ARRAY OBJECT in
-    // order to render ANYTHING.
-    // An object that stores vertex array bindings
-    glGenVertexArrays(1, (GLuint *) &gl_VAO);
-    glBindVertexArray(gl_VAO); // make the VAO the active one ..
-
-    // Upload only one cube's quads since the gl_model is the same for all cubes
-    Cube cube = Cube();
-
-    GLuint gl_VBO;
-    glGenBuffers(1, &gl_VBO);              // generate a 'name for the object'
-    glBindBuffer(GL_ARRAY_BUFFER, gl_VBO); // bind/make VBO the active object
-    glBufferData(GL_ARRAY_BUFFER, cube.byte_size_of_vertices(), NULL,
-                 GL_STATIC_DRAW); // reserve a large buffer for cube quads
-
-    auto vector = cube.to_floats();
-    glBufferSubData(GL_ARRAY_BUFFER, 0, cube.byte_size_of_vertices(), vector.data());
-
-    // Then set our vertex attributes pointers, only doable AFTER linking
-    GLuint positionAttrib = glGetAttribLocation(shaders.at(ShaderType::STANDARD_SHADER).gl_program, "position");
-    glVertexAttribPointer(positionAttrib, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>),
-                          (const void *)offsetof(Vertex<float>, position));
-    glEnableVertexAttribArray(positionAttrib);
-
-    GLuint colorAttrib = glGetAttribLocation(shaders.at(ShaderType::STANDARD_SHADER).gl_program, "vColor");
-    glVertexAttribPointer(colorAttrib, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>),
-                          (const void *)offsetof(Vertex<float>, color));
-    glEnableVertexAttribArray(colorAttrib);
-
-    // Buffer for all the model matrices
-    glGenBuffers(1, (GLuint *) &gl_modelsBO);
-    glBindBuffer(GL_ARRAY_BUFFER, gl_modelsBO);
-
-    GLuint modelAttrib = glGetAttribLocation(shaders.at(ShaderType::STANDARD_SHADER).gl_program, "model");
-    for (int i = 0; i < 4; i++) {
-        glVertexAttribPointer(modelAttrib + i, 4, GL_FLOAT, GL_FALSE, sizeof(Mat4<float>),
-                              (const void *) (sizeof(float) * i * 4));
-        glEnableVertexAttribArray(modelAttrib + i);
-        glVertexAttribDivisor(modelAttrib + i, 1);
-    }
-
-    // View / camera space
-    this->gl_camera_view = glGetUniformLocation(shaders.at(ShaderType::STANDARD_SHADER).gl_program, "camera_view");
-
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube.byte_size_of_indices(), cube.indices.data(), GL_STATIC_DRAW);
-
     // Camera
     auto position  = Vec3<float>{0.0f, 0.0f, 0.0f};  // cam position
     auto direction = Vec3<float>{0.0f, 0.0f, -1.0f}; // position of where the cam is looking
@@ -235,7 +158,7 @@ Renderer::Renderer(): DRAW_DISTANCE(200), projection_matrix(Mat4<float>()), stat
     this->camera = std::make_shared<Camera>(position, direction, world_up);
 }
 
-bool Render::point_inside_frustrum(Vec3<float> point, std::array<Plane<float>, 6> planes) {
+bool Renderer::point_inside_frustrum(Vec3<float> point, std::array<Plane<float>, 6> planes) {
     auto left_plane = planes[0]; auto right_plane = planes[1];
     auto top_plane  = planes[2]; auto bot_plane   = planes[3];
     auto near_plane = planes[4]; auto far_plane   = planes[5];
@@ -249,7 +172,10 @@ bool Render::point_inside_frustrum(Vec3<float> point, std::array<Plane<float>, 6
     return false;
 }
 
-void Render::render_world(const World *world) {
+void Renderer::render() {
+    /// Reset render stats
+    state = RenderState();
+
     /// Frustrum planes
     auto camera_view = FPSViewRH(camera->position, camera->pitch, camera->yaw);
     auto frustrum_view = camera_view * projection_matrix;
@@ -266,77 +192,38 @@ void Render::render_world(const World *world) {
     for (auto batch : graphics_batches) {
         glBindVertexArray(batch.gl_VAO);
         glUseProgram(batch.gl_shader_program);
+        glUniformMatrix4fv(batch.gl_camera_view, 1, GL_FALSE, camera_view.data());
+
+        // TODO: Setup glEnables and stuff, gotta set the defaults in GraphicsState too
+//        glEnable(GL_CULL_FACE);
+//        glCullFace(GL_FRONT);
+//        glFrontFace(GL_CCW);
 
         std::vector<Mat4<float>> buffer{};
         for (auto component : batch.components) {
-            // glBindTexture(GL_TEXTURE_CUBE_MAP, textures[component.graphics_state.texture]);
-            glBindTexture(GL_TEXTURE_CUBE_MAP, textures[Texture::SKYBOX]);
+            // Frustrum cullling
+            if (point_inside_frustrum(component.entity->position, planes)) { continue; }
+
+            // Draw distance
+            auto camera_to_cube = camera->position - component.entity->position;
+            if (camera_to_cube.length() >= DRAW_DISTANCE) { continue; }
+
+            glBindTexture(GL_TEXTURE_CUBE_MAP, textures[component.graphics_state.gl_texture]);
 
             Mat4<float> model{};
-            model = model.translate(camera->position + camera->direction * 2);
+            model = model.translate(component.entity->position);
+            model = model.scale(component.entity->scale);
             model = model.transpose();
             buffer.push_back(model);
         }
         glBindBuffer(GL_ARRAY_BUFFER, batch.gl_models_buffer_object);
         glBufferData(GL_ARRAY_BUFFER, buffer.size() * sizeof(Mat4<float>), buffer.data(), GL_DYNAMIC_DRAW);
         glDrawElementsInstanced(GL_TRIANGLES, batch.mesh.indices.size(), GL_UNSIGNED_INT, 0, buffer.size());
+
+        /// Update render stats
+        state.entities += buffer.size();
     }
-
-    /** Render skybox **/
-    glUseProgram((GLuint) shaders.at(ShaderType::SKYBOX_SHADER).gl_program);
-    glBindVertexArray(gl_skybox_VAO);
-    glEnable(GL_CULL_FACE); // cull face
-    glCullFace(GL_FRONT);   // cull back face
-    glFrontFace(GL_CCW);    // GL_CCW for counter clock-wise
-    glBindTexture(GL_TEXTURE_CUBE_MAP, textures[Texture::SKYBOX]);
-
-    glUniformMatrix4fv(gl_skybox_camera, 1, GL_FALSE, camera_view.data());
-    Mat4<float> model{};
-    model = model.translate(camera->position);
-    model = model.scale(skybox.scale);
-    glUniformMatrix4fv(gl_skybox_model, 1, GL_TRUE, model.data());
-    glDrawElements(GL_TRIANGLES, skybox.indices.size(), GL_UNSIGNED_INT, 0);
-
-    /** Render cubes **/
-    glUseProgram((GLuint) shaders.at(ShaderType::STANDARD_SHADER).gl_program);
-    glBindVertexArray(gl_VAO);
-    glEnable(GL_CULL_FACE); // cull face
-    glCullFace(GL_BACK);    // cull back face
-    glFrontFace(GL_CCW);    // GL_CCW for counter clock-wise
-    glUniformMatrix4fv(gl_camera_view, 1, GL_FALSE, camera_view.data());
-
-    std::vector<Mat4<float>> buffer{};
-    for (int j = 0; j < world->chunks.size(); j++) {
-        auto chunk = &world->chunks[j];
-
-        for (int i = 0; i < chunk->numCubes; i++) {
-            auto cube = &chunk->blocks[i];
-
-            // Frustrum cullling
-            if (point_inside_frustrum(cube->position, planes)) { continue; }
-
-            // Draw distance
-            auto camera_to_cube = camera->position - cube->position;
-            if (camera_to_cube.length() >= DRAW_DISTANCE) { continue; }
-
-            glBindTexture(GL_TEXTURE_CUBE_MAP, textures[cube->texture]);
-            // Model - transform_z * transform_y * transform_x * transform_translation * transform_scaling
-            Mat4<float> model{};
-            model = model.scale(cube->scale);
-            model = model.rotate_x(cube->theta_x);
-            model = model.rotate_y(cube->theta_y);
-            model = model.rotate_z(cube->theta_z);
-            model = model.translate(cube->position);
-            model = model.transpose();
-            buffer.push_back(model);
-        }
-    }
-    glBindBuffer(GL_ARRAY_BUFFER, gl_modelsBO);
-    glBufferData(GL_ARRAY_BUFFER, buffer.size() * sizeof(Mat4<float>), buffer.data(), GL_DYNAMIC_DRAW);
-    glDrawElementsInstanced(GL_TRIANGLES, skybox.indices.size(), GL_UNSIGNED_INT, 0, buffer.size());
-
-    /// Update render state
-    state.entities = buffer.size();
+    state.graphic_batches = graphics_batches.size();
 }
 
 Renderer::~Renderer() {
@@ -345,7 +232,7 @@ Renderer::~Renderer() {
 
 /// Returns the planes from the frustrum matrix in order; {left, right, bottom, top, near, far}
 /// See: http://gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf
-std::array<Plane<float>, 6> Render::extract_planes(Mat4<float> mat) {
+std::array<Plane<float>, 6> Renderer::extract_planes(Mat4<float> mat) {
     auto planes = std::array<Plane<float>, 6>();
     auto left_plane  = Plane<float>(mat[3][0] + mat[0][0],
                                     mat[3][1] + mat[0][1],
@@ -399,7 +286,7 @@ uint64_t Renderer::add_to_batch(RenderComponent component) {
     for (auto &batch : graphics_batches) {
         if (batch.hash_id == component.entity->hash_id) {
             batch.components.push_back(component);
-            return;
+            return render_components_id++;
         }
     }
 
@@ -407,9 +294,6 @@ uint64_t Renderer::add_to_batch(RenderComponent component) {
     batch.mesh = Cube(); /// Default to mesh of a cube ...
     batch.gl_shader_program = shaders.at(ShaderType::STANDARD_SHADER).gl_program;
     glUseProgram(batch.gl_shader_program);
-    // glEnable(GL_CULL_FACE); // cull face
-    // glCullFace(GL_BACK);    // cull back face
-    // glFrontFace(GL_CCW);    // GL_CCW for counter clock-wise
 
     glGenVertexArrays(1, (GLuint *) &batch.gl_VAO);
     glBindVertexArray(batch.gl_VAO);
@@ -423,13 +307,11 @@ uint64_t Renderer::add_to_batch(RenderComponent component) {
 
     // Then set our vertex attributes pointers, only doable AFTER linking
     GLuint positionAttrib = glGetAttribLocation(batch.gl_shader_program, "position");
-    glVertexAttribPointer(positionAttrib, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>),
-                          (const void *)offsetof(Vertex<float>, position));
+    glVertexAttribPointer(positionAttrib, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>), (const void *)offsetof(Vertex<float>, position));
     glEnableVertexAttribArray(positionAttrib);
 
     GLuint colorAttrib = glGetAttribLocation(batch.gl_shader_program, "vColor");
-    glVertexAttribPointer(colorAttrib, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>),
-                          (const void *)offsetof(Vertex<float>, color));
+    glVertexAttribPointer(colorAttrib, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex<float>), (const void *)offsetof(Vertex<float>, color));
     glEnableVertexAttribArray(colorAttrib);
 
     // Buffer for all the model matrices
@@ -438,8 +320,7 @@ uint64_t Renderer::add_to_batch(RenderComponent component) {
 
     GLuint modelAttrib = glGetAttribLocation(batch.gl_shader_program, "model");
     for (int i = 0; i < 4; i++) {
-        glVertexAttribPointer(modelAttrib + i, 4, GL_FLOAT, GL_FALSE, sizeof(Mat4<float>),
-                              (const void *) (sizeof(float) * i * 4));
+        glVertexAttribPointer(modelAttrib + i, 4, GL_FLOAT, GL_FALSE, sizeof(Mat4<float>), (const void *) (sizeof(float) * i * 4));
         glEnableVertexAttribArray(modelAttrib + i);
         glVertexAttribDivisor(modelAttrib + i, 1);
     }
@@ -454,10 +335,25 @@ uint64_t Renderer::add_to_batch(RenderComponent component) {
 
     batch.components.push_back(component);
     graphics_batches.push_back(batch);
+
+    return render_components_id++;
 }
 
 void Renderer::remove_from_batch(RenderComponent component) {
     // TODO: Implement
+}
+
+void Renderer::update_render_component(RenderComponent component) {
+    // TODO: This feels like a bad design, gotta rework it.
+    for (auto &batch : graphics_batches) {
+        if (batch.hash_id == component.entity->hash_id) {
+            for (auto &old_component : batch.components) {
+                if (component.id == old_component.id) {
+                    old_component = component;
+                }
+            }
+        }
+    }
 }
 
 
