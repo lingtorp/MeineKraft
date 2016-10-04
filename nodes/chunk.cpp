ec8d52e6398f6b732249cadd56a02da5a5512ee1
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-04 21:17:34 +0200

    Chunk now uses uniq_ptr to manage lifetime of Blocks

--- nodes/chunk.cpp
+++ nodes/chunk.cpp
@@ -1,31 +1,35 @@
 #include "chunk.h"
+#include <iostream>
+#include "../math/noise.h"
 
 /// @param world_position World coordinates
-Chunk::Chunk(Vec3<float> world_position, const Noise *noise):
+Chunk::Chunk(Vec3<float> world_position, const Noise &noise):
         position(world_position), center_position{world_position.x + dimension/2,
                                                   world_position.y + dimension/2,
                                                   world_position.z + dimension/2},
-                                                  blocks{}, numCubes(0) {
+                                                  blocks{}, num_blocks(0) {
     static auto bottom = -32;
     for (size_t x = 0; x < dimension; x++) {
         for (size_t z = 0; z < dimension; z++) {
             auto X = x + position.x;
             auto Z = z + position.z;
-            auto height = std::round(noise->octaves_of_perlin2d(X, Z, 2, 3, world_position, dimension));
+            auto height = std::round(noise.octaves_of_perlin2d(X, Z, 2, 3, world_position, dimension));
             // std::cout << "Height: " << height;
             // std::cout << " / Noise: " << noise.perlin(X, Z, world_position, dimension);
             // std::cout << " for (x, z) = (" << X << ", " << Z << ")" << std::endl;
             for (auto y = bottom; y < height; y++) {
-                Cube cube = Cube();
-                cube.position.x = x + position.x;
-                cube.position.y = y + position.y;
-                cube.position.z = z + position.z;
-                cube.center = Vec3<float>{cube.position.x + 0.5f,
-                                          cube.position.y + 0.5f,
-                                          cube.position.z + 0.5f};
-                cube.radius = 1.0;
-                blocks.push_back(cube);
-                numCubes++;
+                auto block = std::make_unique<Block>();
+                block->position.x = x + position.x;
+                block->position.y = y + position.y;
+                block->position.z = z + position.z;
+                blocks.push_back(std::move(block));
+                num_blocks++;
+                /*
+                block->center = Vec3<float>{block->position.x + 0.5f,
+                                            block->position.y + 0.5f,
+                                            block->position.z + 0.5f};
+                block.radius = 1.0;
+                 */
             }
         }
     }
--- nodes/chunk.h
+++ nodes/chunk.h
@@ -3,19 +3,18 @@
 
 #include "../math/vector.h"
 #include "../render/primitives.h"
-#include "../math/noise.h"
-#include <cmath>
-#include <iostream>
+#include "block.h"
+
+class Noise;
 
 class Chunk {
 public:
-    Chunk(Vec3<float> world_position, const Noise *noise);
+    Chunk(Vec3<float> world_position, const Noise &noise);
     static const uint16_t dimension = 32; // The 'width' of the chunk in number of cubes
     Vec3<float> position;
     Vec3<float> center_position;
-    uint16_t numCubes;
-    std::vector<Cube> blocks;
-    bool will_be_removed;
+    uint16_t num_blocks;
+    std::vector<std::unique_ptr<Block>> blocks;
 };
 
 #endif //MEINEKRAFT_CHUNK_H
