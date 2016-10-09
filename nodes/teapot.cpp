b1c80173251caf51146738fc19758b524d4494fe
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-09 19:32:42 +0200

    Debugging stuff..

--- nodes/teapot.cpp
+++ nodes/teapot.cpp
@@ -1,7 +1,9 @@
 #include "teapot.h"
 
-Teapot::Teapot(): Entity(generate_entity_id()), render_comp(RenderComponent(this, "/Users/AlexanderLingtorp/Downloads/teapot/teapot.obj")) {
-
+Teapot::Teapot(): Entity(generate_entity_id()), render_comp(RenderComponent(this, "/Users/AlexanderLingtorp/Downloads/dragon.obj")) {
+    // /Users/AlexanderLingtorp/Downloads/dragon.obj
+    // "/Users/AlexanderLingtorp/Downloads/teapot/teapot.obj"
+    scale = 200;
 }
 
 void Teapot::update(uint64_t delta, const std::shared_ptr<Camera> camera) {
