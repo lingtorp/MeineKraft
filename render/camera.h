ce8b6e5a412322e82a815e37f95b65172cbcf157
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-09 18:32:21 +0200

    Increased camera acceleration

--- render/camera.h
+++ render/camera.h
@@ -13,7 +13,7 @@ public:
 
     Camera(Vec3<float> position, Vec3<float> direction, Vec3<float> world_up):
             position(position), direction(direction), up(world_up),
-            pitch(0), yaw(0), velocity(0), acceleration(0.02), max_acceleration(2) {};
+            pitch(0), yaw(0), velocity(0), acceleration(0.03), max_acceleration(3) {};
 
     Vec3<float> move_forward(double delta);
     Vec3<float> move_backward(double delta);
