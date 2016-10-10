13672f6cfac3b28be190cd64cc54289be1d2b2af
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-10 23:45:42 +0200

    Added file monitoring util - watching shader files!

---
+++ render/filemonitor.cpp
@@ -0,0 +1,79 @@
+#include "filemonitor.h"
+#include <sys/stat.h>
+#include <iostream>
+#include <thread>
+
+FileMonitor::FileMonitor(): modified_files{}, monitoring(false), watched_files{}, internal_lock{},
+                            files_modification_time{} {}
+
+void FileMonitor::poll_files() {
+    using namespace std::chrono;
+    while (true) {
+        internal_lock.lock();
+        if (!monitoring) { internal_lock.unlock(); break; }
+        for (auto filepath : watched_files) {
+            struct stat stats;
+            stat(filepath.c_str(), &stats);
+            if (files_modification_time.count(filepath) == 0 ) { // First time checked
+                files_modification_time[filepath] = (uint64_t) stats.st_mtimespec.tv_sec;
+                continue;
+            }
+            auto old_timestamp = files_modification_time[filepath];
+            auto new_timestamp = stats.st_mtimespec.tv_sec;
+            if (old_timestamp == new_timestamp) { continue; }
+            modified_files[filepath] = true;
+            files_modification_time[filepath] = (uint64_t) stats.st_mtimespec.tv_sec;
+            std::cout << stats.st_mtimespec.tv_sec << std::endl;
+        }
+        internal_lock.unlock();
+        std::this_thread::sleep_for(milliseconds(500));
+    }
+}
+
+bool FileMonitor::add_file(std::string filepath) {
+    internal_lock.lock();
+    watched_files.push_back(filepath);
+    internal_lock.unlock();
+}
+
+void FileMonitor::clear_modification_flag(std::string filepath) {
+    internal_lock.lock();
+    modified_files[filepath] = false;
+    internal_lock.unlock();
+}
+
+void FileMonitor::clear_all_modification_flags() {
+    internal_lock.lock();
+    for (auto filepath : modified_files) {
+        modified_files[filepath.first] = false;
+    }
+    internal_lock.unlock();
+}
+
+void FileMonitor::end_monitor() {
+    if (monitoring) {
+        internal_lock.lock();
+        monitoring = false;
+        internal_lock.unlock();
+    }
+}
+
+void FileMonitor::start_monitor() {
+    if (!monitoring) {
+        monitoring = true;
+        std::thread t1{[=]{ poll_files(); }};
+        t1.detach();
+    }
+}
+
+std::vector<std::string> FileMonitor::get_modified_files() {
+    internal_lock.lock();
+    std::vector<std::string> modified_files_to_return{};
+    for (auto file : modified_files) {
+        if (file.second) {
+            modified_files_to_return.push_back(file.first);
+        }
+    }
+    internal_lock.unlock();
+    return modified_files_to_return;
+}
---
+++ render/filemonitor.h
@@ -0,0 +1,47 @@
+#ifndef MEINEKRAFT_FILEMONITOR_H
+#define MEINEKRAFT_FILEMONITOR_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <mutex>
+
+class FileMonitor {
+public:
+    /// Watches all the files in all added to it
+    FileMonitor();
+
+    /// Starts checking files for modification
+    void start_monitor();
+
+    /// Ends checking files for modification
+    void end_monitor();
+
+    /// Adds a file to the FileMonitor if there exists such a file
+    bool add_file(std::string filepath);
+
+    /// Clears the modification flag for one file
+    void clear_modification_flag(std::string filepath);
+
+    /// Clears all the modification flags for all the files
+    void clear_all_modification_flags();
+
+    /// Returns a vector of all the filepaths that was modified recently
+    std::vector<std::string> get_modified_files();
+
+private:
+    bool monitoring;
+    std::mutex internal_lock;
+    void poll_files();
+
+    /// Map of files with their modification flags
+    std::unordered_map<std::string, bool> modified_files;
+
+    /// Filepaths watched
+    std::vector<std::string> watched_files;
+
+    /// Filepaths and their modification time in seconds
+    std::unordered_map<std::string, uint64_t> files_modification_time;
+};
+
+#endif //MEINEKRAFT_FILEMONITOR_H
