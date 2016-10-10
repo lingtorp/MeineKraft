f8fca18f45dd95ce8fdf4c0e12c54ba61f6ac8de
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-10 15:29:14 +0200

    Removed unused field

--- render/graphicsbatch.h
+++ render/graphicsbatch.h
@@ -13,8 +13,6 @@ public:
     Mesh mesh;
     std::vector<RenderComponent> components;
 
-    uint64_t gl_cull_mode; //
-
     uint64_t gl_VAO;
     uint64_t gl_models_buffer_object;
     uint64_t gl_camera_view;
