931bd2b426c8cdfded79c0dbdaf1a145db336dfb
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-13 22:30:51 +0200

    Shaders are now able to recompile

--- render/shader.cpp
+++ render/shader.cpp
@@ -3,22 +3,24 @@
 #include <GL/glew.h>
 #include <iostream>
 #include <fstream>
+#include <SDL_log.h>
 
 Shader::Shader(std::string vertex_filepath, std::string fragment_filepath): vertex_filepath(vertex_filepath),
                                                                             fragment_filepath(fragment_filepath),
+                                                                            vertex_shader(0),
+                                                                            fragment_shader(0),
                                                                             gl_program(0) {}
 
-/// Loads and compiles the shader source, return compile error message in the pair.
 std::pair<bool, std::string> Shader::compile() {
     auto vertex_src   = load_shader_source(vertex_filepath);
     auto fragment_src = load_shader_source(fragment_filepath);
 
     auto raw_str = vertex_src.c_str();
-    uint64_t vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &raw_str, NULL);
 
     raw_str = fragment_src.c_str();
-    uint64_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &raw_str, NULL);
 
     uint64_t shader_program = glCreateProgram();
@@ -34,6 +36,9 @@ std::pair<bool, std::string> Shader::compile() {
     GLint fragment_shader_status;
     glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_shader_status);
 
+    // Always detach shaders after a successful link.
+    glDetachShader(gl_program, vertex_shader);
+    glDetachShader(gl_program, fragment_shader);
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
@@ -52,7 +57,70 @@ std::pair<bool, std::string> Shader::compile() {
     return {false, frag_err_msg + vert_err_msg};
 };
 
-/// Assumes the file exists and will seg. fault otherwise.
+std::pair<bool, std::string> Shader::recompile() {
+    auto vertex_src   = load_shader_source(vertex_filepath);
+    auto fragment_src = load_shader_source(fragment_filepath);
+
+    glDetachShader(gl_program, vertex_shader);
+    glDetachShader(gl_program, fragment_shader);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    vertex_shader   = glCreateShader(GL_VERTEX_SHADER);
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    auto raw_str = vertex_src.c_str();
+    glShaderSource(vertex_shader, 1, &raw_str, NULL);
+
+    raw_str = fragment_src.c_str();
+    glShaderSource(fragment_shader, 1, &raw_str, NULL);
+
+    glCompileShader(vertex_shader);
+    glCompileShader(fragment_shader);
+
+    GLint vertex_shader_status;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_shader_status);
+
+    GLint fragment_shader_status;
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_shader_status);
+
+    if (vertex_shader_status == 1 && fragment_shader_status == 1) {
+        // Relink shader program
+        glAttachShader(gl_program, vertex_shader);
+        glAttachShader(gl_program, fragment_shader);
+        glLinkProgram(gl_program);
+        GLint is_linked = 0;
+        glGetProgramiv(gl_program, GL_LINK_STATUS, &is_linked);
+        SDL_Log("Shader relinking is success: %i", is_linked == GL_TRUE);
+
+        if (is_linked) {
+            // Always detach shaders after a successful link.
+            glDetachShader(gl_program, vertex_shader);
+            glDetachShader(gl_program, fragment_shader);
+            glDeleteShader(vertex_shader);
+            glDeleteShader(fragment_shader);
+        }
+
+        std::string err_log = "";
+        GLint max_log_lng = 0;
+        glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &max_log_lng);
+        err_log.reserve(max_log_lng);
+        glGetProgramInfoLog(gl_program, max_log_lng, NULL, (char *) err_log.c_str());
+        SDL_Log("%s", err_log.c_str());
+
+        return {true, ""};
+    }
+
+    uint64_t err_size = 512;
+    std::string frag_err_msg = "";
+    std::string vert_err_msg = "";
+    frag_err_msg.reserve(err_size);
+    vert_err_msg.reserve(err_size);
+    glGetShaderInfoLog(fragment_shader, err_size, NULL, (char *) frag_err_msg.c_str());
+    glGetShaderInfoLog(vertex_shader  , err_size, NULL, (char *) vert_err_msg.c_str());
+    return {false, frag_err_msg + vert_err_msg};
+};
+
 const std::string Shader::load_shader_source(std::string filename) const {
     std::ifstream ifs(filename);
     return std::string((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
--- render/shader.h
+++ render/shader.h
@@ -7,13 +7,21 @@ class Shader {
 public:
     Shader(std::string vertex_filepath, std::string fragment_filepath);
 
+    /// Loads and compiles the shader source, return compile error message in the pair.
     std::pair<bool, std::string> compile();
+
+    /// Loads and recompiles both shaders
+    std::pair<bool, std::string> recompile();
 
     std::string vertex_filepath;
     std::string fragment_filepath;
     uint64_t gl_program;
 
 private:
+    uint64_t vertex_shader;
+    uint64_t fragment_shader;
+
+    /// Assumes the file exists and will seg. fault otherwise.
     const std::string load_shader_source(std::string filename) const;
 };
 
