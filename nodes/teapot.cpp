99cb84bb71af5bd54201e4741af040c2ca99223c
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-10-26 16:41:29 +0200

    Removed unused methods in Teapot

--- nodes/teapot.cpp
+++ nodes/teapot.cpp
@@ -1,17 +1,11 @@
 #include "teapot.h"
 
-Teapot::Teapot(): Entity(generate_entity_id()), render_comp(RenderComponent(this, "/Users/AlexanderLingtorp/Downloads/dragon.obj")) {
+static std::string file = "/Users/AlexanderLingtorp/Downloads/dragon.obj";
+
+Teapot::Teapot(): Entity(generate_entity_id()), render_comp(RenderComponent(this, file)) {
     // /Users/AlexanderLingtorp/Downloads/dragon.obj
     // "/Users/AlexanderLingtorp/Downloads/teapot/teapot.obj"
+    // "/Users/AlexanderLingtorp/Downloads/dabrovic-sponza/sponza.obj"
     scale = 20;
+    render_comp.set_cube_map_texture(Texture::GRASS);
 }
-
-void Teapot::update(uint64_t delta, const std::shared_ptr<Camera> camera) {
-
-}
-
-Teapot::~Teapot() {
-
-}
-
-
--- nodes/teapot.h
+++ nodes/teapot.h
@@ -7,9 +7,6 @@
 class Teapot: public Entity {
 public:
     Teapot();
-    ~Teapot();
-
-    virtual void update(uint64_t delta, const std::shared_ptr<Camera> camera) override;
 
     RenderComponent render_comp;
 
