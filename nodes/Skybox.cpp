c36ced0117bc74ba8fe2a7a2a930a1df90c2611b
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2017-03-07 10:44:16 +0100

    Cleaned up Skybox following Entity changes

--- nodes/Skybox.cpp
+++ nodes/Skybox.cpp
@@ -1,18 +1,22 @@
-#include <iostream>
 #include "Skybox.h"
 #include "../render/camera.h"
+#include "../render/rendercomponent.h"
 
-Skybox::Skybox(): Entity(), render_comp{this} {
-//    render_comp.set_cube_map_texture(Texture::SKYBOX);
-    scale = 300;
-}
-
-Skybox::~Skybox() {
-    // TODO: Dealloc RenderComp
+Skybox::Skybox(): Entity() {
+    scale = 10;
+    auto render_comp = new RenderComponent(this);
+    render_comp->set_mesh(MeshPrimitive::Cube);
+    const auto base = "/Users/AlexanderLingtorp/Google Drive/Repositories/MeineKraft/";
+    std::vector<std::string> faces = {base + std::string("res/sky/right.jpg"),
+                                      base + std::string("res/sky/left.jpg"),
+                                      base + std::string("res/sky/top.jpg"),
+                                      base + std::string("res/sky/bottom.jpg"),
+                                      base + std::string("res/sky/back.jpg"),
+                                      base + std::string("res/sky/front.jpg")};
+    render_comp->set_cube_map_texture(faces);
+    attach_component(render_comp);
 }
 
 void Skybox::update(uint64_t delta, const std::shared_ptr<Camera> camera) {
-    /// Update all components
     position = camera->position;
-    // render_comp.update(delta)
 }
--- nodes/Skybox.h
+++ nodes/Skybox.h
@@ -2,21 +2,12 @@
 #define MEINEKRAFT_SKYBOX_H
 
 #include "entity.h"
-#include "../render/rendercomponent.h"
 
 class Skybox: public Entity {
 public:
     Skybox();
-    ~Skybox();
 
     virtual void update(uint64_t delta, const std::shared_ptr<Camera> camera) override;
-
-    RenderComponent render_comp;
-
-    constexpr uint64_t generate_entity_id() {
-        // TODO: Iterate generate the next entity id
-        return 0;
-    }
 };
 
 #endif //MEINEKRAFT_SKYBOX_H
