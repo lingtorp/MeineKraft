2a0303b3996a5ef14f3eeca5b46196861e2376be
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2017-03-11 17:47:57 +0100

    Removed duplicate model creation

--- main.cpp
+++ main.cpp
@@ -44,26 +44,8 @@ int main() {
     Skybox skybox{};
 
     std::string directory = "/Users/AlexanderLingtorp/Desktop/";
-    std::string falu_stuga = "/Users/AlexanderLingtorp/Desktop/falu-stuga.obj";
-    std::string dragon = "/Users/AlexanderLingtorp/Desktop/dragon.obj";
-    std::string teapot = "/Users/AlexanderLingtorp/Desktop/teapot.obj";
-
-    Teapot teapot0{falu_stuga, directory};
-
-    Teapot teapot1{falu_stuga, directory};
-    teapot1.position = {50, 0, 0};
-
-    Teapot teapot2{falu_stuga, directory};
-    teapot2.position = {50, 0, 50};
-
-    Teapot teapot3{falu_stuga, directory};
-    teapot3.position = {0, 0, 50};
-
-    Teapot *new_teapot;
-    for (int i = 0; i < 4; i++) {
-        new_teapot = new Teapot{falu_stuga, directory};
-        new_teapot->position = Vec3<float>{i * 50.0f, 0, i * 50.0f};
-    }
+    std::string model_file = "/Users/AlexanderLingtorp/Desktop/falu-stuga3.obj";
+    Teapot model{model_file, directory};
 
     bool DONE = false;
     uint32_t last_tick = SDL_GetTicks(), current_tick, delta;
