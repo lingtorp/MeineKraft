718b572e2c2589b851ec55197f40a4b53c15b552
Alexander Lingtorp <alexander@lingtorp.com>
2018-01-11 09:11:21 +0100

    Texture filtering defaults are not to be relied on

--- render/render.cpp
+++ render/render.cpp
@@ -133,6 +133,10 @@ Renderer::Renderer(): DRAW_DISTANCE(200), projection_matrix(Mat4<float>()), stat
   glActiveTexture(GL_TEXTURE0 + gl_ssao_texture_unit);
   glGenTextures(1, &gl_ssao_texture);
   glBindTexture(GL_TEXTURE_2D, gl_ssao_texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, screen_width, screen_height, 0, GL_RED, GL_FLOAT, nullptr);
   glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, gl_ssao_texture, 0);
   
