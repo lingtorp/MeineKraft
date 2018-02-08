1adedea89b7cdf6f499398fe739d6654839ca8fa
Alexander Lingtorp <AlexanderLingtorp@n175-p183.eduroam.kth.se>
2018-02-08 11:03:23 +0100

    Simplified MeshManager a lot. Unclear future for our manager.

--- render/meshmanager.h
+++ render/meshmanager.h
@@ -1,7 +1,6 @@
 #ifndef MEINEKRAFT_MESHMANAGER_HPP
 #define MEINEKRAFT_MESHMANAGER_HPP
 
-#include <unordered_map>
 #include "primitives.h"
 
 struct MeshInformation {
@@ -9,32 +8,16 @@ struct MeshInformation {
     std::string loaded_from_filepath;
 };
 
-/**
- * Loads, stores, Meshes used by the Renderer
- */
-class MeshManager {
-private:
-    std::unordered_map<uint64_t, MeshInformation> meshes;
-public:
-    MeshManager();
-
-    /// Return the mesh_id if flag is true
-    std::pair<uint64_t, bool> is_mesh_loaded(std::string filepath, std::string directory);
-
-    /**
-     * Loads a model file and its textures, blocking.
-     * @param filepath Filepath to the model file to be loaded
-     * @return Mesh ID that identifies the loaded Mesh and a list of Textures (type, filepath).
-     */
-    std::pair<uint64_t, std::vector<std::pair<Texture::Type, std::string>>>
-    load_mesh(std::string directory, std::string file);
-
-    Mesh mesh_from_id(uint64_t mesh_id);
-
-    /// Number of meshes loaded
-    uint64_t meshes_loaded;
-
-    uint64_t mesh_id_from_primitive(MeshPrimitive primitive);
+struct MeshManager {
+  std::vector<Mesh> loaded_meshes{};
+  
+  std::pair<ID, std::vector<std::pair<Texture::Type, std::string>>>
+  load_mesh(std::string directory, std::string file);
+  
+  Mesh mesh_from_id(ID id) {
+    // FIXME: There is no mechanism behind this at all
+    return loaded_meshes[id];
+  }
 };
 
 #endif //MEINEKRAFT_MESHMANAGER_HPP
