378226fe7e3c542c34dff830b64f0eb1c3edde41
Alexander Lingtorp <alexander@lingtorp.com>
2018-05-29 11:31:05 +0200

    Changed Linux hardcoded path to shaders.

--- util/filesystem.h
+++ util/filesystem.h
@@ -5,7 +5,7 @@ namespace FileSystem {
   // TODO: Make these dynamic rather than static
   /// Base filepath to the executeable
   #ifdef __linux__
-  const std::string base = "/home/alex/repos/MeineKraft/";
+  const std::string base = "/home/lingtorp/repos/MeineKraft/";
   #elif __APPLE__
   const std::string base = "/Users/AlexanderLingtorp/Repositories/MeineKraft/";
   #endif
