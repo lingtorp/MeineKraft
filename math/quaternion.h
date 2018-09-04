d9d2bfe1c3a933e0b9a13e1d198bd1e9820bd72d
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-09-04 20:42:08 +0200

    Quaternion implementaiton improved slightly

--- math/quaternion.h
+++ math/quaternion.h
@@ -3,12 +3,18 @@
 
 #include "vector.h"
 
+struct quat;
+
+template<typename T>
+inline quat operator*(const T s, const quat& v);
+
 struct quat {
   Vec3<float> v;
   float w;
 
-  quat(): v(0.0), w(1.0) {}
-  quat(const Vec3<float>& v): v(v), w(1.0) {}
+  explicit quat(): v(0.0), w(1.0) {}
+  explicit quat(const Vec3<float>& v): v(v), w(1.0) {}
+  quat(const Vec3<float>& v, const float w) : v(v), w(w) {}
 
   float norm() const {
     return std::sqrt(v.dot(v) + w * w);
@@ -19,29 +25,35 @@ struct quat {
   }
 
   quat conjugate() const {
-    return {-v, w};
+    return quat{-v, w};
   }
 
   /// Utils
-  Mat4<float> matrix() const {
-    return {};
-  }
-
   /// Rotates point around the quat vector v by deg degrees
-  Vec3<float> rotate(const Vec3<float>& point, const float deg) const {
+  Vec3<float> rotate(const Vec3<float>& point, const float rads) const {
     quat p(point, 1.0);
-    quat q(std::sinf(deg / 2.0f) * v.normalize(), std::cosf(deg / 2.0f));
-    return q * (p * q.inverse());
+    quat q(std::sinf(rads / 2.0f) * v.normalize(), std::cosf(rads / 2.0f));
+    return quat(q * (p * q.inverse())).v;
   }
 
   /// Operators
   inline quat operator*(const quat& r) {
-    return {v.cross(r.v) + r.w * v + w * r.v, w * r.w - v.dot(r.v)};
+    return quat{v.cross(r.v) + r.w * v + w * r.v, w * r.w - v.dot(r.v)};
   }
 
-  inline quat operator*(const float s) {
-    return {s * v, s * w};
+  friend std::ostream &operator<<(std::ostream& os, const quat& q) {
+    return os << "(i:" << q.v.x << " j:" << q.v.y << " k:" << q.v.z << " w:" << q.w << ")" << std::endl;
   }
 };
+
+template<typename T>
+inline quat operator*(const T s, const quat& q) {
+  return quat(s * q.v, s * q.w);
+}
+
+template<typename T>
+inline quat operator*(const quat& q, const T s) {
+  return quat(s * q.v, s * q.w);
+}
 
 #endif // MEINEKRAFT_QUATERNION_H
