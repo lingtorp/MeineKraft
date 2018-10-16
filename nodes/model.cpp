6cc2c1ff25ebaabcbf743d18d85c9059a1f096f8
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-10-16 09:17:51 +0200

    Model now adds a Transform component

--- nodes/model.cpp
+++ nodes/model.cpp
@@ -1,6 +1,10 @@
 #include "model.h"
 
 Model::Model(const std::string& directory, const std::string& file) {
+  TransformComponent transform;
+  transform.position = Vec3f(-2.0f, 0.0f, 0.0f);
+  transform.scale = 1.0f;
+  attach_component(transform);
   RenderComponent render;
   render.set_mesh(directory, file);
   render.set_shading_model(ShadingModel::PhysicallyBased);
