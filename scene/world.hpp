c50a247ffc2c0456f1d4f92f541495f784ec6f4a
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-10-16 09:18:58 +0200

    Updated world

--- scene/world.hpp
+++ scene/world.hpp
@@ -15,22 +15,25 @@
 #include <algorithm>
 
 class Block: public Entity {
-  public:
-    enum class BlockType {
-    AIR, GRASS, DIRT
+public:
+  enum class BlockType {
+    GRASS, DIRT
   };
   
   BlockType type;
   
-  explicit Block(BlockType type = BlockType::AIR): type(type) {
-    if (type == BlockType::AIR) { return; }
+  explicit Block(const Vec3f position, BlockType type): type(type) {
+    TransformComponent transform;
+    transform.position = position;
+    transform.scale = 1.0f;
+    this->attach_component(transform);
     RenderComponent render_comp;
-    render_comp.set_mesh(MeshPrimitive::Cube); // or render_comp.mesh_id = ID(MeshPrimitive::Cube);
+    render_comp.set_mesh(MeshPrimitive::Cube); 
     render_comp.set_cube_map_texture(textures_for_block(type));
-    render_comp.set_shading_model(ShadingModel::Unlit); // or render_comp.shading_model = ShadingModel::Unlit;
+    render_comp.set_shading_model(ShadingModel::Unlit); 
     this->attach_component(render_comp);
   }
-  
+
   static std::vector<std::string> textures_for_block(BlockType type) {
     std::vector<std::string> faces;
     switch (type) {
@@ -56,24 +59,24 @@ class Block: public Entity {
 };
 
 class Chunk {
-  public:
+public:
   static const int dimension = 2;
-  const Vec3<int> world_position;
-  explicit Chunk(Vec3<int> world_position): blocks{std::array<std::array<std::array<Block, dimension>, dimension>, dimension>()}, world_position{world_position} {}
-  
-  const Block* block_at(Vec3<int> position) {
-    if (position < Vec3<int>{dimension}) {
+  const Vec3i world_position;
+  explicit Chunk(Vec3i world_position) : blocks{std::vector<std::vector<std::vector<Block>>>()}, world_position{world_position} {}
+
+  const Block* block_at(const Vec3i& position) {
+    if (position < Vec3i{dimension}) {
       return nullptr;
     }
     return &blocks[position.x][position.y][position.z];
   }
   
-  private:
-  std::array<std::array<std::array<Block, dimension>, dimension>, dimension> blocks;
+private:
+  std::vector<std::vector<std::vector<Block>>> blocks;
 };
 
 struct World {
-  public:
+public:
   std::unordered_map<Vec3<int>, Chunk> chunks;
   
   explicit World(): chunks{} {
@@ -84,29 +87,18 @@ struct World {
     std::iota(X.begin(), X.end(), -10);
     for (const auto x : X) {
       Block::BlockType block_type = distr(engine) < 0.5 ? Block::BlockType::GRASS : Block::BlockType::DIRT;
-      Block* block = new Block(block_type);
-      // TransformComponent transform;
-      // transform.position = Vec3<float>{ -15.0f + 2.5f * j, -15.0f + 2.5f * i, -5.0f }; 
-      // block->attach_component(transform);
+      Block* block = new Block(Vec3f(0.0f, 0.0f, 1.0f + 1.0f * x), block_type);
     }
 
     for (size_t i = 0; i < 10; i++) {
       for (size_t j = 0; j < 10; j++) {
-        /*
-        EntitySystem es;
-        ComponentFlag components = ComponentFlag::GraphicsState | ComponentFlag::Name;
-        Entity entity = es.create_entity_with(components); // Entity == ID
-        es.objects.positions[es.index_of_entity(entity, ComponentFlag::Position)] = Vec3f{-15.0f + 2.5f * j, -15.0f + 2.5f * i, -5.0f};
-        */
-        //
-
         Entity* entity = new Entity();
-        // TransformComponent transform;
-        // transform.position = Vec3<float>{ -15.0f + 2.5f * j, -15.0f + 2.5f * i, -5.0f }; 
-        // entity->attach_component(transform);
+        TransformComponent transform;
+        transform.position = Vec3f{ -15.0f + 2.5f * j, -15.0f + 2.5f * i, -5.0f }; 
+        entity->attach_component(transform);
         RenderComponent render;
         render.set_mesh(MeshPrimitive::Sphere);
-        render.pbr_scalar_parameters = Vec3<float>(0.1 * i, 0.1, 0.0);
+        render.pbr_scalar_parameters = Vec3f(0.1 * i, 0.1, 0.0);
         render.set_shading_model(ShadingModel::PhysicallyBasedScalars);
         entity->attach_component(render);
       }
@@ -114,8 +106,8 @@ struct World {
   }
   
   /// World position is measured in Chunk lengths
-  Vec3<float> world_position(const Vec3<float>& position) const {
-    Vec3<float> result{};
+  Vec3f world_position(const Vec3f& position) const {
+    Vec3f result{};
     result.x = std::round(position.x / Chunk::dimension) * Chunk::dimension;
     result.y = std::round(position.y / Chunk::dimension) * Chunk::dimension;
     result.z = std::round(position.z / Chunk::dimension) * Chunk::dimension;
