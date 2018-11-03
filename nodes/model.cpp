37ce51797fa330d82491044c22626923e7f2525c
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-11-03 18:55:56 +0100

    Update nodes/model.cpp

--- nodes/model.cpp
+++ nodes/model.cpp
@@ -2,7 +2,7 @@
 
 Model::Model(const std::string& directory, const std::string& file) {
   TransformComponent transform;
-  transform.position = Vec3f(-2.0f, 0.0f, 0.0f);
+  transform.position = Vec3f(-2.0f, 2.0f, 0.0f);
   transform.scale = 1.0f;
   attach_component(transform);
   RenderComponent render;
