7e88fbe9600d5b38f0e9a1b0fad8964454fa3e20
Alexander Lingtorp <Entalpi@users.noreply.github.com>
2018-11-03 18:57:15 +0100

    Future real TextureManager ...

---
+++ render/texturemanager.h
@@ -0,0 +1,10 @@
+#ifndef MEINEKRAFT_TEXTUREMANAGER_HPP
+#define MEINEKRAFT_TEXTUREMANAGER_HPP
+
+#include "primitives.h"
+#include "texture.h"
+
+#include <unordered_map>
+
+
+#endif // MEINEKRAFT_TEXTUREMANAGER_HPP
