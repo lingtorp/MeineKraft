61730579901e40a0a03788a4ffc8d0c28276391b
Alexander Lingtorp <alexander@lingtorp.com>
2019-05-03 20:00:08 +0200

    Performance enchancements to texture loading

--- src/rendering/texture.hpp
+++ src/rendering/texture.hpp
@@ -44,49 +44,40 @@ struct TextureResource {
 };
 
 struct Texture {
-  static RawTexture load_textures(const TextureResource resource) {
+  static RawTexture load_textures(const TextureResource& resource) {
     RawTexture texture{};
 
     if (resource.files.empty()) { return texture; }
 
-    const auto& file = resource.files.front();
-    SDL_Surface* image = IMG_Load(file.c_str());
-
-    if (image) {
+    // Load all the files into a linear memory region
+    // NOTE: Assumes that the files are the same size, in right order, same encoding, etc
+    for (size_t i = 0; i < resource.files.size(); i++) {
+      SDL_Surface* image = IMG_Load(resource.files[i].c_str());
+      if (!image) {
+        Log::error("Could not load texture: " + std::string(IMG_GetError()));
+        continue;
+      }
       texture.width = static_cast<uint32_t>(image->w);
       texture.height = static_cast<uint32_t>(image->h);
       texture.bytes_per_pixel = image->format->BytesPerPixel;
-      SDL_FreeSurface(image); // FIXME: Wasteful
-
-      // Assumes that the files are the same size, in right order, same encoding, etc
       texture.size = texture.bytes_per_pixel * texture.width * texture.height;
       texture.pixels = static_cast<uint8_t*>(std::calloc(1, texture.size * resource.files.size()));
 
-      // Load all the files into a linear memory region
-      for (size_t i = 0; i < resource.files.size(); i++) {
-        image = IMG_Load(resource.files[i].c_str());
-        if (!image) {
-          Log::error("Could not load texture: " + std::string(IMG_GetError()));
-          continue;
-        }
-        if (image->w != texture.width || image->h != texture.height) {
-          Log::error("Textures non-uniform size");
-          return texture;
-        }
-        // Convert it to OpenGL friendly format
-        const auto desired_img_format = texture.bytes_per_pixel == 3 ? SDL_PIXELFORMAT_RGB24 : SDL_PIXELFORMAT_RGBA32;
+      // Convert it to OpenGL friendly format if needed
+      const auto desired_img_format = texture.bytes_per_pixel == 3 ? SDL_PIXELFORMAT_RGB24 : SDL_PIXELFORMAT_RGBA32;
+      if (image->format->format == desired_img_format) {
+        std::memcpy(texture.pixels + texture.size * i, image->pixels, texture.size);
+      } else {
         SDL_Surface* conv = SDL_ConvertSurfaceFormat(image, desired_img_format, 0);
         if (conv) {
           std::memcpy(texture.pixels + texture.size * i, conv->pixels, texture.size);
-          SDL_FreeSurface(image);
+          SDL_FreeSurface(conv);
         } else {
           Log::error("RawTexture loading failed: " + std::string(SDL_GetError()));
         }
-
-        texture.faces++;
       }
-    } else {
-      Log::error("Could not load textures: " + std::string(IMG_GetError()));
+      SDL_FreeSurface(image);
+      texture.faces++;
     }
 
     return texture;
