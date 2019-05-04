57d7cd7a4186199577baa9c7ca03e16e9904a622
Alexander Lingtorp <alexander@lingtorp.com>
2019-05-04 13:34:37 +0200

    Increased Pointlight intensities ...

--- src/rendering/light.hpp
+++ src/rendering/light.hpp
@@ -9,7 +9,7 @@ struct PointLight {
   Vec4f position;   // (X, Y, X, padding)
   Vec4f intensity;  // (R, G, B, padding)
 
-  explicit PointLight(Vec3f position): position(position), intensity(230.47f, 210.31f, 200.79f) {};
+  explicit PointLight(Vec3f position): position(position), intensity(23000.47f, 21000.31f, 20000.79f) {};
 
   friend std::ostream &operator<<(std::ostream &os, const PointLight &light) { return os << light.position; }
 };
