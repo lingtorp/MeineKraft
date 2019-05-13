4e150bdd19c1c70d52387568c9a67f1cfd621ad6
Alexander Lingtorp <alexander@lingtorp.com>
2019-05-13 20:52:30 +0200

    Update model.hpp

--- src/nodes/model.hpp
+++ src/nodes/model.hpp
@@ -5,7 +5,7 @@
 #include "entity.hpp"
 #include "../rendering/rendercomponent.hpp"
 
-class Model : public Entity {
+class Model: public Entity {
 public:
   Model(const std::string& directory, const std::string& file);
   Model(const RenderComponent& render);
