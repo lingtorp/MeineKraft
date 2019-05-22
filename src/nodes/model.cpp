3755095da57ecdddb0a7bc9a37ca8e26e2a94f84
Alexander Lingtorp <alexander@lingtorp.com>
2019-05-22 17:06:38 +0200

    Update model.cpp

--- src/nodes/model.cpp
+++ src/nodes/model.cpp
@@ -4,7 +4,7 @@
 Model::Model(const std::string& directory, const std::string& file) {
   NameSystem::instance().add_name_to_entity(file, id);
   TransformComponent transform;
-  transform.position = Vec3f(-2.0f, 2.0f, 0.0f);
+  transform.position = Vec3f(0.0f, 0.0f, 0.0f);
   transform.scale = 1.0f;
   attach_component(transform);
   RenderComponent render;
