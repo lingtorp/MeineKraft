f3b2f5f07bdf2eace006854485a0d4a84d9cd091
Alexander Lingtorp <alexander@lingtorp.com>
2020-01-06 14:29:49 +0100

    Rewrote Shader for improved error handling, etc

--- src/rendering/shader.cpp
+++ src/rendering/shader.cpp
@@ -7,273 +7,17 @@
 #include <glew.h>
 #else
 #include <GL/glew.h>
-#endif 
-
-#if defined(__APPLE__)
-static const char* GLSL_VERSION = "#version 410 core \n";
-#elif defined(__linux__)
-static const char* GLSL_VERSION = "#version 450 core \n";
-#elif defined(WIN32)
-static const char* GLSL_VERSION = "#version 460 core \n";
 #endif
 
-ComputeShader::ComputeShader(const std::string& compute_filepath,
-                             const std::vector<std::string>& defines) {
-  GLuint gl_comp_shader = glCreateShader(GL_COMPUTE_SHADER);
-  std::string comp_src = Filesystem::read_file(compute_filepath);
+#if defined(__APPLE__)
+static const char *GLSL_VERSION = "#version 410 core \n";
+#elif defined(__linux__)
+static const char *GLSL_VERSION = "#version 450 core \n";
+#elif defined(WIN32)
+static const char *GLSL_VERSION = "#version 460 core \n";
+#endif
 
-  if (comp_src.empty()) {
-    Log::error("Tried to compile empty compute shader .."); exit(-1);
-  }
-
-  for (const std::string& define : defines) {
-    comp_src.insert(0, define);
-  }
-
-  // Insert the define into the shader src
-  comp_src.insert(0, GLSL_VERSION);
-
-  const char* raw_str_ptr = comp_src.c_str();
-  glShaderSource(gl_comp_shader, 1, &raw_str_ptr, nullptr);
-  glCompileShader(gl_comp_shader);
-
-  glObjectLabel(GL_SHADER, gl_comp_shader, -1, compute_filepath.c_str());
-
-  gl_program = glCreateProgram();
-  glAttachShader(gl_program, gl_comp_shader);
-  glLinkProgram(gl_program);
-
-  glObjectLabel(GL_PROGRAM, gl_program, -1, compute_filepath.c_str());
-
-  GLint compute_shader_status = 0;
-  glGetShaderiv(gl_comp_shader, GL_COMPILE_STATUS, &compute_shader_status);
-
-  glDetachShader(gl_program, gl_comp_shader);
-  glDeleteShader(gl_comp_shader);
-
-  if (compute_shader_status == GL_FALSE) {
-    Log::error("Could not compile compute shader, aborting ... ");
-
-    GLint err_size = 0;
-    glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &err_size);
-    char* prog_err_msg = new char[err_size];
-    glGetProgramInfoLog(gl_program, err_size, nullptr, prog_err_msg);
-    if (err_size != 0) {
-      Log::error(compute_filepath + std::string(prog_err_msg));
-    }
-    delete[] prog_err_msg;
-
-    exit(-1);
-  }
-}
-
-Shader::Shader(const std::string& vert_shader_file,
-               const std::string& geom_shader_file,
-               const std::string& frag_shader_file) {
-  std::string vertex_src   = Filesystem::read_file(vert_shader_file);
-  std::string geometry_src = Filesystem::read_file(geom_shader_file);
-  std::string fragment_src = Filesystem::read_file(frag_shader_file);
-
-  if (vertex_src.empty()) {
-    Log::error("Vertex shader passed could not be opened or is empty");
-    return;
-  }
-  if (geometry_src.empty()) {
-    Log::error("Geometry shader passed could not be opened or is empty");
-    return;
-  }
-  if (fragment_src.empty()) {
-    Log::error("Fragment shader passed could not be opened or is empty");
-    return;
-  }
-
-  vertex_src.insert(0, GLSL_VERSION);
-  geometry_src.insert(0, GLSL_VERSION);
-  fragment_src.insert(0, GLSL_VERSION);
-
-  auto raw_str = vertex_src.c_str();
-  gl_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(gl_vertex_shader, 1, &raw_str, nullptr);
-
-  raw_str = geometry_src.c_str();
-  gl_geometry_shader = glCreateShader(GL_GEOMETRY_SHADER);
-  glShaderSource(gl_geometry_shader, 1, &raw_str, nullptr);
-
-  raw_str = fragment_src.c_str();
-  gl_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(gl_fragment_shader, 1, &raw_str, nullptr);
-
-  gl_program = glCreateProgram();
-  glCompileShader(gl_vertex_shader);
-  glCompileShader(gl_geometry_shader);
-  glCompileShader(gl_fragment_shader);
-  glAttachShader(gl_program, gl_vertex_shader);
-  glAttachShader(gl_program, gl_geometry_shader);
-  glAttachShader(gl_program, gl_fragment_shader);
-  glLinkProgram(gl_program);
-
-  glObjectLabel(GL_SHADER, gl_vertex_shader, -1, vert_shader_file.c_str());
-  glObjectLabel(GL_SHADER, gl_geometry_shader, -1, geom_shader_file.c_str());
-  glObjectLabel(GL_SHADER, gl_fragment_shader, -1, frag_shader_file.c_str());
-  glObjectLabel(GL_PROGRAM, gl_program, -1, frag_shader_file.c_str());
-
-  GLint vertex_shader_status = 0;
-  glGetShaderiv(gl_vertex_shader, GL_COMPILE_STATUS, &vertex_shader_status);
-
-  GLint geometry_shader_status = 0;
-  glGetShaderiv(gl_geometry_shader, GL_COMPILE_STATUS, &geometry_shader_status);
-
-  GLint fragment_shader_status = 0;
-  glGetShaderiv(gl_fragment_shader, GL_COMPILE_STATUS, &fragment_shader_status);
-
-  GLint err_size = 0;
-  glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &err_size);
-  char* prog_err_msg = new char[err_size];
-  glGetProgramInfoLog(gl_program, err_size, nullptr, prog_err_msg);
-	if (err_size != 0) { Log::error(vert_shader_file + " / " + frag_shader_file + "\n" + prog_err_msg); }
-  delete[] prog_err_msg;
-
-  if (vertex_shader_status == GL_TRUE && fragment_shader_status == GL_TRUE && geometry_shader_status == GL_TRUE) {    
-    glDetachShader(gl_program, gl_vertex_shader);
-    glDetachShader(gl_program, gl_geometry_shader);
-    glDetachShader(gl_program, gl_fragment_shader);
-    glDeleteShader(gl_vertex_shader);
-    glDeleteShader(gl_geometry_shader);
-    glDeleteShader(gl_fragment_shader);
-
-    compiled_successfully = true;
-  } else {
-    GLint err_size = 0;
-    glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &err_size);
-
-    char* prog_err_msg = new char[err_size];
-    glGetProgramInfoLog(gl_program, err_size, nullptr, prog_err_msg);
-    Log::error(vert_shader_file + " / " + frag_shader_file + prog_err_msg);
-    delete[] prog_err_msg;
-    
-    char* vert_err_msg = new char[err_size];
-    glGetShaderInfoLog(gl_vertex_shader, err_size, nullptr, vert_err_msg);
-    Log::error(vert_shader_file + vert_err_msg);
-    delete[] vert_err_msg;
-
-    glGetShaderiv(gl_geometry_shader, GL_INFO_LOG_LENGTH, &err_size);
-    char* geom_err_msg = new char[err_size];
-    glGetShaderInfoLog(gl_geometry_shader, err_size, nullptr, geom_err_msg);
-    Log::error(geom_shader_file + geom_err_msg);
-    delete[] geom_err_msg;
-
-    glGetShaderiv(gl_fragment_shader, GL_INFO_LOG_LENGTH, &err_size);
-    char* frag_err_msg = new char[err_size];
-    glGetShaderInfoLog(gl_fragment_shader, err_size, nullptr, frag_err_msg);
-    Log::error(frag_shader_file + frag_err_msg);
-    delete[] frag_err_msg;
-
-    log_gl_error();
-
-    glDetachShader(gl_program, gl_vertex_shader);
-    glDetachShader(gl_program, gl_geometry_shader);
-    glDetachShader(gl_program, gl_fragment_shader);
-    glDeleteShader(gl_vertex_shader);
-    glDeleteShader(gl_geometry_shader);
-    glDeleteShader(gl_fragment_shader);
-    glDeleteProgram(gl_program);
-  }
-}
-
-Shader::Shader(const std::string& vertex_filepath, const std::string& fragment_filepath):
-  vertex_filepath(vertex_filepath), fragment_filepath(fragment_filepath),
-  gl_vertex_shader(0), gl_fragment_shader(0), gl_program(0), defines{} {}
-
-std::pair<bool, std::string> Shader::compile() {
-  if (!Filesystem::file_exists(vertex_filepath) || !Filesystem::file_exists(fragment_filepath)) {
-    return {false, "File(s) do not exists: " + vertex_filepath + " and/or " + fragment_filepath};
-  }
-
-  vertex_src   = Filesystem::read_file(vertex_filepath);
-  fragment_src = Filesystem::read_file(fragment_filepath);
-
-  if (vertex_src.empty()) {
-    return {false, "Vertex shader passed could not be opened or is empty"};
-  }
-  if (fragment_src.empty()) {
-    return {false, "Fragment shader passed could not be opened or is empty"};
-  }
-
-  for (const auto& define : defines) {
-    vertex_src.insert(0, Shader::shader_define_to_string(define));
-    fragment_src.insert(0, Shader::shader_define_to_string(define));
-  }
-
-  vertex_src.insert(0, GLSL_VERSION);
-  fragment_src.insert(0, GLSL_VERSION);
-
-  auto raw_str = vertex_src.c_str();
-  gl_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(gl_vertex_shader, 1, &raw_str, nullptr);
-
-  raw_str = fragment_src.c_str();
-  gl_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(gl_fragment_shader, 1, &raw_str, nullptr);
-  
-  GLint gl_shader_program = glCreateProgram();
-  glCompileShader(gl_vertex_shader);
-  glCompileShader(gl_fragment_shader);
-  glAttachShader(gl_shader_program, gl_vertex_shader);
-  glAttachShader(gl_shader_program, gl_fragment_shader);
-
-  glObjectLabel(GL_SHADER, gl_vertex_shader, -1, vertex_filepath.c_str());
-  glObjectLabel(GL_SHADER, gl_fragment_shader, -1, fragment_filepath.c_str());
-  glObjectLabel(GL_PROGRAM, gl_shader_program, -1, fragment_filepath.c_str());
-
-  GLint vertex_shader_status = 0;
-  glGetShaderiv(gl_vertex_shader, GL_COMPILE_STATUS, &vertex_shader_status);
-
-  GLint fragment_shader_status = 0;
-  glGetShaderiv(gl_fragment_shader, GL_COMPILE_STATUS, &fragment_shader_status);
-
-  if (vertex_shader_status == GL_TRUE && fragment_shader_status == GL_TRUE) {
-    glLinkProgram(gl_shader_program);
-
-    glDetachShader(gl_shader_program, gl_vertex_shader);
-    glDetachShader(gl_shader_program, gl_fragment_shader);
-    glDeleteShader(gl_vertex_shader);
-    glDeleteShader(gl_fragment_shader);
-    gl_program = gl_shader_program;
-    return {true, ""};
-  } else {
-    GLint err_size = 0;
-    glGetShaderiv(gl_vertex_shader, GL_INFO_LOG_LENGTH, &err_size);
-    
-    char* vert_err_msg = new char[err_size];
-    glGetShaderInfoLog(gl_vertex_shader, err_size, nullptr, vert_err_msg);
-    Log::info(vert_err_msg);
-    delete[] vert_err_msg;
-
-    glGetShaderiv(gl_fragment_shader, GL_INFO_LOG_LENGTH, &err_size);
-    char* frag_err_msg = new char[err_size];
-    glGetShaderInfoLog(gl_fragment_shader, err_size, nullptr, frag_err_msg);
-    Log::info(frag_err_msg);
-    delete[] frag_err_msg;
-
-    glGetProgramiv(gl_shader_program, GL_INFO_LOG_LENGTH, &err_size);
-    char* prog_err_msg = new char[err_size];
-    glGetProgramInfoLog(gl_shader_program, err_size, nullptr, prog_err_msg);
-    Log::info(prog_err_msg);
-    delete[] prog_err_msg;
-
-    log_gl_error();
-
-    glDetachShader(gl_shader_program, gl_vertex_shader);
-    glDetachShader(gl_shader_program, gl_fragment_shader);
-    glDeleteShader(gl_vertex_shader);
-    glDeleteShader(gl_fragment_shader);
-    glDeleteProgram(gl_shader_program);
-
-    return {false, std::string(vert_err_msg) + std::string(frag_err_msg) + std::string(prog_err_msg)};
-  }
-};
-
-std::string Shader::shader_define_to_string(const Shader::Defines define) {
+static std::string shader_define_to_string(const Shader::Defines define) {
   switch (define) {
   case Shader::Defines::Diffuse2D:
     return "#define DIFFUSE_2D \n";
@@ -294,10 +38,266 @@ std::string Shader::shader_define_to_string(const Shader::Defines define) {
   }
 }
 
-void Shader::add(const Shader::Defines define) {
-  defines.insert(define);
+ComputeShader::ComputeShader(const std::string &compute_filepath,
+                             const std::vector<std::string> &defines) {
+  GLuint gl_comp_shader = glCreateShader(GL_COMPUTE_SHADER);
+  std::string comp_src = Filesystem::read_file(compute_filepath);
+
+  if (comp_src.empty()) {
+    Log::error("Tried to compile empty compute shader ..");
+    exit(-1);
+  }
+
+  for (const std::string &define : defines) {
+    comp_src.insert(0, define);
+  }
+
+  // Insert the define into the shader src
+  comp_src.insert(0, GLSL_VERSION);
+
+  const char *raw_str_ptr = comp_src.c_str();
+  glShaderSource(gl_comp_shader, 1, &raw_str_ptr, nullptr);
+  glCompileShader(gl_comp_shader);
+
+  glObjectLabel(GL_SHADER, gl_comp_shader, -1, compute_filepath.c_str());
+
+  gl_program = glCreateProgram();
+  glAttachShader(gl_program, gl_comp_shader);
+  glLinkProgram(gl_program);
+
+  glObjectLabel(GL_PROGRAM, gl_program, -1, compute_filepath.c_str());
+
+  GLint compute_shader_status = 0;
+  glGetShaderiv(gl_comp_shader, GL_COMPILE_STATUS, &compute_shader_status);
+
+  glDetachShader(gl_program, gl_comp_shader);
+  glDeleteShader(gl_comp_shader);
+
+  if (compute_shader_status == GL_FALSE) {
+    GLint err_size = 0;
+    glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &err_size);
+    char *prog_err_msg = new char[err_size];
+    glGetProgramInfoLog(gl_program, err_size, nullptr, prog_err_msg);
+    if (err_size != 0) {
+      Log::error("Could not compile compute shader: " + compute_filepath + "\n" + std::string(prog_err_msg));
+    }
+    delete[] prog_err_msg;
+
+    exit(-1);
+  }
 }
 
-bool Shader::operator==(const Shader &rhs) {
- return defines == rhs.defines;
+Shader::Shader(const std::string &vertex_filepath,
+               const std::string &geometry_filepath,
+               const std::string &fragment_filepath)
+    : vertex_filepath(vertex_filepath), geometry_filepath(geometry_filepath),
+      fragment_filepath(fragment_filepath),
+      vertex_src(Filesystem::read_file(vertex_filepath)),
+      geometry_src(Filesystem::read_file(geometry_filepath)),
+      fragment_src(Filesystem::read_file(fragment_filepath)) {}
+
+Shader::Shader(const std::string &vertex_filepath,
+               const std::string &fragment_filepath)
+    : vertex_filepath(vertex_filepath), fragment_filepath(fragment_filepath),
+      vertex_src(Filesystem::read_file(vertex_filepath)),
+      fragment_src(Filesystem::read_file(fragment_filepath)) {}
+
+std::pair<bool, std::string> Shader::compile() {
+  const bool vertex_included = !vertex_filepath.empty();
+  const bool geometry_included = !geometry_filepath.empty();
+  const bool fragment_included = !fragment_filepath.empty();
+
+  gl_program = glCreateProgram();
+
+  GLint vertex_shader_compiled = GL_FALSE;
+  GLint geometry_shader_compiled = GL_FALSE;
+  GLint fragment_shader_compiled = GL_FALSE;
+
+  if (!vertex_filepath.empty()) {
+    if (vertex_src.empty()) {
+      return {false, "Vertex shader passed could not be opened or is empty"};
+    }
+
+    for (const auto &define : defines) {
+      vertex_src.insert(0, shader_define_to_string(define));
+    }
+
+    vertex_src.insert(0, GLSL_VERSION);
+
+    const auto raw_str = vertex_src.c_str();
+    gl_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(gl_vertex_shader, 1, &raw_str, nullptr);
+
+    glCompileShader(gl_vertex_shader);
+    glAttachShader(gl_program, gl_vertex_shader);
+
+    glObjectLabel(GL_SHADER, gl_vertex_shader, -1, vertex_filepath.c_str());
+
+    glGetShaderiv(gl_vertex_shader, GL_COMPILE_STATUS, &vertex_shader_compiled);
+
+    if (vertex_shader_compiled == GL_FALSE) {
+      const auto err_msg =
+          "Vertex shader (" + vertex_filepath + ") failed to compile";
+      return {false, err_msg};
+    }
+  }
+
+  if (!geometry_filepath.empty()) {
+    if (geometry_src.empty()) {
+      return {false, "Geometry shader passed could not be opened or is empty"};
+    }
+
+    for (const auto &define : defines) {
+      geometry_src.insert(0, shader_define_to_string(define));
+    }
+
+    geometry_src.insert(0, GLSL_VERSION);
+
+    const auto raw_str = geometry_src.c_str();
+    gl_geometry_shader = glCreateShader(GL_GEOMETRY_SHADER);
+    glShaderSource(gl_geometry_shader, 1, &raw_str, nullptr);
+
+    glCompileShader(gl_geometry_shader);
+    glAttachShader(gl_program, gl_geometry_shader);
+
+    glObjectLabel(GL_SHADER, gl_geometry_shader, -1, geometry_filepath.c_str());
+
+    glGetShaderiv(gl_geometry_shader, GL_COMPILE_STATUS,
+                  &geometry_shader_compiled);
+
+    if (geometry_shader_compiled == GL_FALSE) {
+      const auto err_msg =
+          "Geometry shader (" + geometry_filepath + ") failed to compile";
+      return {false, err_msg};
+    }
+  }
+
+  if (!fragment_filepath.empty()) {
+    if (fragment_src.empty()) {
+      return {false, "Fragment shader passed could not be opened or is empty"};
+    }
+
+    for (const auto &define : defines) {
+      fragment_src.insert(0, shader_define_to_string(define));
+    }
+
+    fragment_src.insert(0, GLSL_VERSION);
+
+    const auto raw_str = fragment_src.c_str();
+    gl_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(gl_fragment_shader, 1, &raw_str, nullptr);
+
+    glCompileShader(gl_fragment_shader);
+    glAttachShader(gl_program, gl_fragment_shader);
+
+    glObjectLabel(GL_SHADER, gl_fragment_shader, -1, fragment_filepath.c_str());
+
+    glGetShaderiv(gl_fragment_shader, GL_COMPILE_STATUS,
+                  &fragment_shader_compiled);
+
+    if (fragment_shader_compiled == GL_FALSE) {
+      const auto err_msg =
+          "Fragment shader (" + fragment_filepath + ") failed to compile";
+      return {false, err_msg};
+    }
+  }
+
+  const std::string program_label =
+      (vertex_included ? vertex_filepath + ", " : "") +
+      (geometry_included ? geometry_filepath + ", " : "") +
+      (fragment_included ? fragment_filepath + ", " : "");
+  glObjectLabel(GL_PROGRAM, gl_program, -1, program_label.c_str());
+
+  const bool all_shaders_compiled =
+      (vertex_included ? vertex_shader_compiled : true) &&
+      (geometry_included ? geometry_shader_compiled : true) &&
+      (fragment_included ? fragment_shader_compiled : true);
+
+  if (!all_shaders_compiled) {
+    GLint err_size = 0;
+    char *err_msg = nullptr;
+
+    std::string total_err_msg = "";
+
+    if (vertex_included) {
+      glGetShaderiv(gl_vertex_shader, GL_INFO_LOG_LENGTH, &err_size);
+
+      err_msg = new char[err_size];
+      glGetShaderInfoLog(gl_vertex_shader, err_size, nullptr, err_msg);
+      total_err_msg += std::string(err_msg);
+      delete[] err_msg;
+
+      glDetachShader(gl_program, gl_vertex_shader);
+      glDeleteShader(gl_vertex_shader);
+    }
+
+    if (geometry_included) {
+      glGetShaderiv(gl_fragment_shader, GL_INFO_LOG_LENGTH, &err_size);
+      err_msg = new char[err_size];
+      glGetShaderInfoLog(gl_fragment_shader, err_size, nullptr, err_msg);
+      total_err_msg += std::string(err_msg);
+      delete[] err_msg;
+
+      glDetachShader(gl_program, gl_geometry_shader);
+      glDeleteShader(gl_geometry_shader);
+    }
+
+    if (fragment_included) {
+      glGetShaderiv(gl_fragment_shader, GL_INFO_LOG_LENGTH, &err_size);
+      err_msg = new char[err_size];
+      glGetShaderInfoLog(gl_fragment_shader, err_size, nullptr, err_msg);
+      total_err_msg += std::string(err_msg);
+      delete[] err_msg;
+
+      glDetachShader(gl_program, gl_fragment_shader);
+      glDeleteShader(gl_fragment_shader);
+    }
+
+    glGetProgramiv(gl_program, GL_INFO_LOG_LENGTH, &err_size);
+    err_msg = new char[err_size];
+    glGetProgramInfoLog(gl_program, err_size, nullptr, err_msg);
+    total_err_msg += std::string(err_msg);
+    delete[] err_msg;
+
+    glDeleteProgram(gl_program);
+
+    return {false, total_err_msg};
+  }
+
+  glLinkProgram(gl_program);
+
+  if (vertex_included) {
+    glDetachShader(gl_program, gl_vertex_shader);
+    glDeleteShader(gl_vertex_shader);
+  }
+
+  if (geometry_included) {
+    glDetachShader(gl_program, gl_geometry_shader);
+    glDeleteShader(gl_geometry_shader);
+  }
+
+  if (fragment_included) {
+    glDetachShader(gl_program, gl_fragment_shader);
+    glDeleteShader(gl_fragment_shader);
+  }
+
+  return {true, ""};
+};
+
+void Shader::add(const Shader::Defines define) { defines.insert(define); }
+
+void Shader::add(const std::string &str) {
+  if (!vertex_filepath.empty()) {
+    vertex_src.insert(0, str);
+  }
+
+  if (!geometry_filepath.empty()) {
+    geometry_src.insert(0, str);
+  }
+
+  if (!fragment_filepath.empty()) {
+    fragment_src.insert(0, str);
+  }
 }
+
+bool Shader::operator==(const Shader &rhs) { return defines == rhs.defines; }
--- src/rendering/shader.hpp
+++ src/rendering/shader.hpp
@@ -2,53 +2,65 @@
 #ifndef MEINEKRAFT_SHADER_HPP
 #define MEINEKRAFT_SHADER_HPP
 
+#include <set>
 #include <string>
 #include <vector>
-#include <set>
 
 struct Shader {
-  enum class Defines: uint32_t {
-    Diffuse2D,        // OpenGL texture target (GL_TEXTURE_2D)
-    DiffuseCubemap,   // OpenGL texture target (GL_TEXTURE_CUBE_MAP)
-    DiffuseRGB,       // Diffuse texture format 24 bit depth
-    DiffuseRGBA,      // Diffuse texture format 32 bit depth
-    Emissive          // Has emissive texture 
+
+  enum class Defines : uint32_t {
+    Diffuse2D,      // OpenGL texture target (GL_TEXTURE_2D)
+    DiffuseCubemap, // OpenGL texture target (GL_TEXTURE_CUBE_MAP)
+    DiffuseRGB,     // Diffuse texture format 24 bit depth
+    DiffuseRGBA,    // Diffuse texture format 32 bit depth
+    Emissive        // Has emissive texture
   };
-  // Configuration of the shader a la Ubershader
-  std::set<Shader::Defines> defines;
 
   Shader() = default;
-  Shader(const std::string& vert_shader_file,
-         const std::string& frag_shader_file);
-  Shader(const std::string& vert_shader_file,
-         const std::string& geom_shader_file,
-         const std::string& frag_shader_file);
+  Shader(const std::string &vert_shader_file,
+         const std::string &frag_shader_file);
+  Shader(const std::string &vert_shader_file,
+         const std::string &geom_shader_file,
+         const std::string &frag_shader_file);
 
-  bool operator==(const Shader& rhs);
+  // Add shader config. definition
+  void add(const Shader::Defines define);
 
-  /// Loads and compiles the shader source, return compile error message in the pair.
+  // Adds raw string to the shader sources
+  void add(const std::string &str);
+
+  /// Loads and compiles shader sources returns compile error msg
   std::pair<bool, std::string> compile();
 
-  static std::string shader_define_to_string(Shader::Defines define);
-  void add(Shader::Defines define);
+  // Filepaths
+  // NOTE: Shader stage considered active if it is not .empty()
+  std::string vertex_filepath = "";
+  std::string geometry_filepath = "";
+  std::string fragment_filepath = "";
 
-  std::string vertex_filepath;
-  std::string fragment_filepath;
-  std::string vertex_src;
-  std::string fragment_src;
-  
+  // Shader source
+  std::string vertex_src = "";
+  std::string geometry_src = "";
+  std::string fragment_src = "";
+
   uint32_t gl_program = 0;
-  uint32_t gl_vertex_shader   = 0;
+  uint32_t gl_vertex_shader = 0;
   uint32_t gl_geometry_shader = 0;
   uint32_t gl_fragment_shader = 0;
 
+  // Set by compile function, gets stale if the shader is changed
   bool compiled_successfully = false;
+
+  bool operator==(const Shader &rhs);
+
+  // Configuration of the shader a la Ubershader
+  std::set<Shader::Defines> defines{};
 };
 
 struct ComputeShader {
-  ComputeShader(const std::string& compute_filepath,
-                const std::vector<std::string>& defines = {});
-  uint32_t gl_program;
+  explicit ComputeShader(const std::string &compute_filepath,
+                         const std::vector<std::string> &defines = {});
+  uint32_t gl_program = 0;
 };
 
 #endif // MEINEKRAFT_SHADER_HPP
