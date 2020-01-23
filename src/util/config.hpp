3b5328802a7eb5aee177a736c761c08faceed6bd
Alexander Lingtorp <alexander@lingtorp.com>
2020-01-23 10:17:13 +0100

    Updated config loading

--- src/util/config.hpp
+++ src/util/config.hpp
@@ -2,11 +2,12 @@
 
 #include "../../include/json/json.hpp"
 
-using json = nlohmann::json;
-
 struct Scene;
 
+/// Configuration of MeineKraft
+/// Governed by config.json in root project folder
+/// Please see documentation in docs/
 struct Config {
-  static json load_config(bool& success);
+  static nlohmann::json load_config(bool& success);
   static void save_scene(const Scene* scene);
 };
