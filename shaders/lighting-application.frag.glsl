6e17e54b4c67ec18af04c73e684cc62492bc1555
Alexander Lingtorp <alexander@lingtorp.com>
2020-01-24 16:37:45 +0100

    Conditional lighting application due to upsamling

--- shaders/lighting-application.frag.glsl
+++ shaders/lighting-application.frag.glsl
@@ -9,6 +9,11 @@ uniform sampler2D uSpecular_radiance; // RGB (float)
 uniform sampler2D uAmbient_radiance;  // R   (float)
 uniform sampler2D uDirect_radiance;   // RGB (float)
 
+// Depending on the pipeline settings
+uniform bool uIndirect_radiance_applicable;
+uniform bool uSpecular_radiance_applicable;
+uniform bool uAmbient_radiance_applicable;
+
 uniform vec2 uPixel_size;
 
 out vec3 color;
@@ -17,16 +22,16 @@ void main() {
   const vec2 c = gl_FragCoord.xy * uPixel_size;
 
   // Indirect
-  color += texture(uIndirect_radiance, c).rgb;
+  if (uIndirect_radiance_applicable) { color += texture(uIndirect_radiance, c).rgb; }
 
   // Emissive
   color += texture(uEmissive_radiance, c).rgb;
 
   // Specular
-  color += texture(uSpecular_radiance, c).rgb;
+  if (uSpecular_radiance_applicable) { color += texture(uSpecular_radiance, c).rgb; }
 
   // Ambient
-  color += vec3(texture(uAmbient_radiance, c).r);
+  if (uAmbient_radiance_applicable) { color += vec3(texture(uAmbient_radiance, c).r); }
 
   // Direct
   color += texture(uDirect_radiance, c).rgb;
