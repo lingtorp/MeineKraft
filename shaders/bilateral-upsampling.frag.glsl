bdcd1c2a6bedc6ddc2ddf0d810b30dac8e506c63
Alexander Lingtorp <alexander@lingtorp.com>
2020-02-26 13:49:33 +0100

    Normalmapping in JBU

--- shaders/bilateral-upsampling.frag.glsl
+++ shaders/bilateral-upsampling.frag.glsl
@@ -20,6 +20,11 @@ uniform bool uNormal_weight;
 uniform sampler2D uNormal_high_res;
 uniform sampler2D uNormal_low_res;
 
+// Normal mapping
+uniform bool uNormal_mapping;
+uniform sampler2D uTangent_normal;
+uniform sampler2D uTangent;
+
 uniform bool uDepth_weight;
 uniform sampler2D uDepth_high_res;
 uniform sampler2D uDepth_low_res;
@@ -45,15 +50,22 @@ vec4 get_bilinear_weights(const vec2 p, const vec2 BL) {
   return weights;
 }
 
-// FIXME: Depth is not downsampled currently due to the complexity (see the srcs)
+/// Converts 'depth' to linear: [0, 1]
+float linearize_depth(const float depth) {
+  const float near = 0.1;   // FIXME: Hard coded camera dependent
+  const float far = 3000.0; // FIXME: Hard coded camera dependent
+  const float z = depth * 2.0 - 1.0; // to NDC
+  return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
 /// Depths weights depending on similarity to high-res depth
 /// Src: https://www.ppsloan.org/publications/ProxyPG.pdf
 vec4 get_depth_weights(const vec2 p, const vec2 coarse_samples[4]) {
-  const float fine_depth = texture(uDepth_high_res, p).r;
+  const float fine_depth = linearize_depth(texture(uDepth_high_res, p).r);
 
   vec4 weights;
   for (uint i = 0; i < 4; i++) {
-    const float coarse = texture(uDepth_low_res, coarse_samples[i]).r;
+    const float coarse = linearize_depth(texture(uDepth_low_res, coarse_samples[i]).r);
     const float diff = abs(coarse - fine_depth);
     weights[i] = min(1.0 / (diff + EPSILON), 1.0);
   }
@@ -64,33 +76,71 @@ vec4 get_depth_weights(const vec2 p, const vec2 coarse_samples[4]) {
 /// Normal weights from the coarse samples
 /// Src: https://www.ppsloan.org/publications/ProxyPG.pdf
 vec4 get_normal_weights(const vec2 p, const vec2 coarse_samples[4]) {
-  const vec3 normal = texture(uNormal_high_res, p).xyz;
+  const vec3 fNormal = texture(uNormal_high_res, p).xyz;
+  vec3 normal = fNormal;
+
+  if (uNormal_mapping) {
+    // Tangent normal mapping & TBN tranformation
+    const vec3 T = normalize(texture(uTangent, p).xyz);
+    const vec3 B = normalize(cross(normal, T));
+    const vec3 N = normalize(normal);
+    mat3 TBN = mat3(1.0);
+
+    TBN = mat3(T, B, N);
+    const vec3 tangent_normal = normalize(2.0 * (texture(uTangent_normal, p).xyz - vec3(0.5)));
+    normal = normalize(TBN * tangent_normal);
+  }
 
   vec4 weights;
   for (uint i = 0; i < 4; i++) {
-    const vec3 coarse = texture(uNormal_low_res, coarse_samples[i]).xyz;
-    weights[i] = pow(dot(normal, coarse), 32);
+    const vec3 coarse = normalize(texture(uNormal_low_res, coarse_samples[i]).xyz);
+    weights[i] = min(max(pow(dot(normal, coarse), 32), 0.0), 1.0);
   }
 
   return weights;
 }
 
-// STATUS: Upsamplingen ser i princip ut som nearest neighbor upsampling ..
+/// Computes weights based on the positional aspect of the guide texture
+/// NOTE: Unclear what this actually contributes with to the filtering
+vec4 get_position_weights(const vec2 p, const vec2 coarse_samples[4]) {
+  const vec3 fine = texture(uPosition_high_res, p).xyz;
+
+  vec4 weights;
+  for (uint i = 0; i < 4; i++) {
+    const vec3 coarse = texture(uPosition_low_res, coarse_samples[i]).xyz;
+    const float dist = max(1.0 / (EPSILON + distance(fine, coarse)), 1.0);
+    weights[i] = dist;
+  }
+
+  return weights;
+}
+
 void main() {
-  // Coarse samples: Bottom-left, bottom-right, top-left, top-right
+  // Coarse samples: bottom-left, bottom-right, top-left, top-right
   const vec2 BL = get_bottom_left_low_res_texel(gl_FragCoord.xy);
   const vec2 BR = BL + vec2((1.0 / uInput_texture_size).x, 0.0);
   const vec2 TL = BL + vec2(0.0, (1.0 / uInput_texture_size).y);
-  const vec2 TR = BL + (1.0 / uInput_texture_size);
+  const vec2 TR = BL + vec2(1.0 / uInput_texture_size);
   const vec2 coarse_samples[4] = {BL, BR, TL, TR};
 
-  const vec4 bilinear_weights = get_bilinear_weights(gl_FragCoord.xy, BL);
+  vec4 weights = get_bilinear_weights(gl_FragCoord.xy, BL);
 
   const vec2 p = gl_FragCoord.xy * uOutput_pixel_size; // [0, 1] in high-res
-  // const vec4 depth_weights = get_depth_weights(p, coarse_samples);
-  const vec4 normal_weights = get_normal_weights(p, coarse_samples);
 
-  const vec4 weights = bilinear_weights; // * normal_weights;
+  if (uDepth_weight) {
+    const vec4 depth_weights = get_depth_weights(p, coarse_samples);
+    weights *= depth_weights;
+  }
+
+  if (uNormal_weight) {
+    const vec4 normal_weights = get_normal_weights(p, coarse_samples);
+    weights *= normal_weights;
+  }
+
+  if (uPosition_weight) {
+    const vec4 position_weights = get_position_weights(p, coarse_samples);
+    weights *= position_weights;
+  }
 
   // Samples
   float total_weight = 0.0;
