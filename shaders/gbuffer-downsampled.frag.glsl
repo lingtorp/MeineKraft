a6ba85c54b480c165439cd2c408b26015d98a159
Alexander Lingtorp <alexander@lingtorp.com>
2020-02-26 13:49:41 +0100

    Boxdownsampling of depth

--- shaders/gbuffer-downsampled.frag.glsl
+++ shaders/gbuffer-downsampled.frag.glsl
@@ -1,14 +1,19 @@
+/// Boxdownsamples textures for use in bilateral upsampling
+/// File: gbuffer-downsampled.frag.glsl
 
 uniform vec2 uInput_pixel_size;
 
 uniform sampler2D uPosition;
 uniform sampler2D uNormal;
+uniform sampler2D uDepth;
 
-layout(location = 0) out vec3 gPosition;
-layout(location = 1) out vec3 gNormal;
+layout(location = 0) out vec3  gPosition;
+layout(location = 1) out vec3  gNormal;
+layout(location = 2) out float gDepth;
 
 void main() {
   const vec2 c = gl_FragCoord.xy * uInput_pixel_size;
   gPosition = texture(uPosition, c).xyz;
   gNormal = texture(uNormal, c).xyz;
+  gDepth = texture(uDepth, c).r;
 }
